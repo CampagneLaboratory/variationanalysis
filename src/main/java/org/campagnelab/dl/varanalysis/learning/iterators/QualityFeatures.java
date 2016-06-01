000ec937bf34c2bb87fc255cacc1dec032ff2963
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2016-06-01 14:02:08 -0400

    qualityfeatures adjusted to new qualitygenotypecount

--- src/main/java/org/campagnelab/dl/varanalysis/learning/iterators/QualityFeatures.java
+++ src/main/java/org/campagnelab/dl/varanalysis/learning/iterators/QualityFeatures.java
@@ -1,6 +1,7 @@
 package org.campagnelab.dl.varanalysis.learning.iterators;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.campagnelab.dl.varanalysis.learning.mappers.FeatureMapper;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
@@ -12,7 +13,7 @@ import java.util.Collections;
  * @author Remi Torracinta, rct66
  */
 
-public class QualityFeatures implements FeatureCalculator {
+public class QualityFeatures implements FeatureMapper {
 
 
     public static final int MAX_GENOTYPES = 5;
@@ -30,10 +31,6 @@ public class QualityFeatures implements FeatureCalculator {
         //shouldn't need to do anything
     }
 
-    @Override
-    public int numberOfLabels() {
-        return 20;
-    }
 
     int[] indices = new int[]{0, 0};
 
@@ -46,53 +43,76 @@ public class QualityFeatures implements FeatureCalculator {
         }
     }
 
-
-    @Override
-    public void mapLabels(BaseInformationRecords.BaseInformationOrBuilder record, INDArray labels, int indexOfRecord) {
-        indices[0] = indexOfRecord;
-
-        for (int labelIndex = 0; labelIndex < numberOfLabels(); labelIndex++) {
-            indices[1] = labelIndex;
-            labels.putScalar(indices, produceLabel(record, labelIndex));
-        }
-    }
-
-    /**
-     * Produce the value of a given label for the specified record.
-     *
-     * @param record     The record of interest.
-     * @param labelIndex The index of the label to produce/calculate
-     * @return The value of the label.
-     */
-    @Override
-    public float produceLabel(BaseInformationRecords.BaseInformationOrBuilder record, int labelIndex) {
-        //TODO : implements the method
-        return 0;
-    }
-
-
     public float produceFeature(BaseInformationRecords.BaseInformationOrBuilder record, int featureIndex) {
+        return normalize(produceFeatureInternal(record, featureIndex), sumCounts);
+    }
+
+
+    private float normalize(float value, int normalizationFactor) {
+        if (normalizationFactor == 0) {
+            return 0;
+        }
+        float normalized = value / normalizationFactor;
+        assert normalized >= 0 && normalized <= 1 : "value must be normalized: " + normalized;
+        return normalized;
+    }
+
+
+    public float produceFeatureInternal(BaseInformationRecords.BaseInformationOrBuilder record, int featureIndex) {
         assert featureIndex >= 0 && featureIndex < MAX_GENOTYPES * 2*2 : "Only MAX_GENOTYPES*2*2 features";
         if (featureIndex < MAX_GENOTYPES * 2) {
             // germline counts written first:
             if ((featureIndex % 2) == 1) {
                 // odd featureIndices are forward strand:
-                return record.getSamples(0).getCounts(featureIndex/2).getQualityScoreForwardStrand();
+                return getAllCounts(record, false).get(featureIndex / 2).getQualityScoreForward();
             } else {
-                return record.getSamples(0).getCounts(featureIndex/2).getQualityScoreReverseStrand();
+                return getAllCounts(record, false).get(featureIndex / 2).getQualityScoreReverse();
             }
         } else {
             // tumor counts written next:
             featureIndex -= MAX_GENOTYPES * 2;
             if ((featureIndex % 2) == 1) {
                 // odd featureIndices are forward strand:
-                return record.getSamples(1).getCounts(featureIndex/2).getQualityScoreForwardStrand();
+                return getAllCounts(record, true).get(featureIndex / 2).getQualityScoreForward();
             } else {
-                return record.getSamples(1).getCounts(featureIndex/2).getQualityScoreReverseStrand();
+                return getAllCounts(record, true).get(featureIndex / 2).getQualityScoreReverse();
             }
         }
     }
 
+    private ObjectArrayList<QualityGenotypeCount> getAllCounts(BaseInformationRecords.BaseInformationOrBuilder record, boolean isTumor) {
+        assert oneSampleHasTumor(record.getSamplesList()) : "at least one sample must have hasTumor=true.";
+        ObjectArrayList<QualityGenotypeCount> list = new ObjectArrayList();
+        for (BaseInformationRecords.SampleInfo sampleInfo : record.getSamplesList()) {
+            if (isTumor != sampleInfo.getIsTumor()) continue;
+            for (BaseInformationRecords.CountInfo sampleCounts : sampleInfo.getCountsList()) {
+                QualityGenotypeCount count = new QualityGenotypeCount(
+                        sampleCounts.getGenotypeCountForwardStrand(),
+                        sampleCounts.getGenotypeCountReverseStrand(),
+                        sampleCounts.getToSequence(),
+                        sampleCounts.getQualityScoreForwardStrand(),
+                        sampleCounts.getQualityScoreReverseStrand());
+                list.add(count);
+            }
+        }
+        // pad with zero until we have 10 elements:
+        while (list.size() < MAX_GENOTYPES) {
+            list.add(new QualityGenotypeCount(0, 0, "N", 0, 0));
+        }
+        // trim the list at 5 elements because we will consider only the 5 genotypes with largest total counts:
+        list.trim(MAX_GENOTYPES);
+        //sort in decreasing order of counts:
+        Collections.sort(list);
+        return list;
+    }
 
+
+    private boolean oneSampleHasTumor(java.util.List<org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords.SampleInfo> samples) {
+        for (BaseInformationRecords.SampleInfo sample : samples) {
+            if (sample.getIsTumor()) return true;
+        }
+        return false;
+
+    }
  }
 
