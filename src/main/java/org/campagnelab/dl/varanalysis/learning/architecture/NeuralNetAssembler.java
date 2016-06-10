b81aa8629f2de7680a177b263d7418f73a67b4c3
Fabien Campagne <fac2003@campagnelab.org>
2016-06-10 18:20:57 -0400

    Add skeleton classes for NeuralNetAssemblers.

---
+++ src/main/java/org/campagnelab/dl/varanalysis/learning/architecture/NeuralNetAssembler.java
@@ -0,0 +1,9 @@
+package org.campagnelab.dl.varanalysis.learning.architecture;
+
+/**
+ * An interface for classes that assemble neural network configuration according to some architecture.
+ * Created by fac2003 on 6/10/16.
+ * @author Fabien Campagne
+ */
+public interface NeuralNetAssembler {
+}
