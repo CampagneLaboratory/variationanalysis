92678036fd85b4640bdb15958155312c9d858e35
Fabien Campagne <fac2003@campagnelab.org>
2016-06-11 11:54:53 -0400

    ParquetPrinter: add the option to provide a refIndex and position to print only records for those sites. Add a test that compares the features for mutated and plain site on a specific locus where a large failure occurs with a trained network.

--- src/main/java/org/campagnelab/dl/varanalysis/storage/ParquetPrinter.java
+++ src/main/java/org/campagnelab/dl/varanalysis/storage/ParquetPrinter.java
@@ -1,8 +1,6 @@
 package org.campagnelab.dl.varanalysis.storage;
 
 
-
-
 import com.codahale.metrics.MetricRegistryListener;
 import com.google.protobuf.TextFormat;
 import org.apache.parquet.hadoop.ParquetReader;
@@ -16,47 +14,70 @@ import java.util.List;
 
 
 /**
- *
- *
  * Currently holds the main method. Jar takes two arguments
- *
- *  java -jar var-analysis.jar process /path/to/genotypes.parquet
- *  creates mutated and randomized parquet file, then prints the latter
- *
- *  java -jar var-analysis.jar print /path/to/genotypes.parquet
- *  prints the parquet file as is
- *
+ * <p>
+ * java -jar var-analysis.jar process /path/to/genotypes.parquet
+ * creates mutated and randomized parquet file, then prints the latter
+ * <p>
+ * java -jar var-analysis.jar print /path/to/genotypes.parquet
+ * prints the parquet file as is
+ * <p>
  * Also, this jar should be a resource for Goby to output variations as a parquet file using its AvroVariationOutputFormat
  * class.
  * Created by rct66 on 5/17/16.
+ *
  * @author rct66
  */
 public class ParquetPrinter {
 
     String path;
+    boolean focusPrint = false;
+    private int refIndex;
+    private int position;
 
-    public static void main(String[] args) throws IOException{
-        new ParquetPrinter(args[0]).print();
+    public static void main(String[] args) throws IOException {
+        if (args.length<1) {
+            System.err.println("usage: printer <parquet-file> [focus-ref-index focus-position]");
+            System.exit(1);
+        }
+        ParquetPrinter parquetPrinter = new ParquetPrinter(args[0]);
+        if (args.length >= 3) {
+            // will only print the record(s) matching a specific position:
+            parquetPrinter.setFocusOnPosition(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            System.out.println("Scanning for ");
+        }
+        parquetPrinter.print();
 
     }
 
+    private void setFocusOnPosition(int refIndex, int position) {
+        focusPrint = true;
+        this.refIndex = refIndex;
+        this.position = position;
+    }
 
-    public ParquetPrinter(String path){
+
+    public ParquetPrinter(String path) {
         this.path = path;
     }
 
-    private void recordPrinter(BaseInformationRecords.BaseInformation base) throws IOException{
+    private void recordPrinter(BaseInformationRecords.BaseInformation base) throws IOException {
         TextFormat.print(base, System.out);
     }
 
     public void print() {
         try {
             RecordReader reader = new RecordReader(path);
-            for (BaseInformationRecords.BaseInformation base : reader){
-                recordPrinter(base);
+            for (BaseInformationRecords.BaseInformation base : reader) {
+                if (!focusPrint ||
+                        (base.getReferenceIndex() == refIndex && base.getPosition() == position)) {
+                    recordPrinter(base);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+
 }
