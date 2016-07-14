8c9d3e693fe62aae5cc8c31159e3305a4017fabf
Fabien Campagne <fac2003@campagnelab.org>
2016-07-14 10:29:36 -0400

    Switch loss function to the cross-entropy. This may help with estimating confidence, need to evaluate.

--- model-training/src/main/java/org/campagnelab/dl/varanalysis/learning/SomaticTrainer.java
+++ model-training/src/main/java/org/campagnelab/dl/varanalysis/learning/SomaticTrainer.java
@@ -93,7 +93,7 @@ public abstract class SomaticTrainer {
         assembler.setNumInputs(numInputs);
         assembler.setNumOutputs(numOutputs);
         assembler.setRegularization(false);
-        lossFunction = LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD;
+        lossFunction = LossFunctions.LossFunction.XENT;
         assembler.setLossFunction(lossFunction);
         // assembler.setRegularizationRate(1e-6);
         // assembler.setDropoutRate(dropoutRate);
