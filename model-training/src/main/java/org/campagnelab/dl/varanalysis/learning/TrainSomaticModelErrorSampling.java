d31e90cd9d73492ec0f1e566804b769904d835c9
Fabien Campagne <fac2003@campagnelab.org>
2016-08-24 11:15:27 -0400

    Add early check of validation set for error sampling.

--- model-training/src/main/java/org/campagnelab/dl/varanalysis/learning/TrainSomaticModelErrorSampling.java
+++ model-training/src/main/java/org/campagnelab/dl/varanalysis/learning/TrainSomaticModelErrorSampling.java
@@ -19,6 +19,7 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
@@ -72,6 +73,13 @@ public class TrainSomaticModelErrorSampling extends SomaticTrainer {
 
     @Override
     protected EarlyStoppingResult<MultiLayerNetwork> train(MultiLayerConfiguration conf, DataSetIterator async) throws IOException {
+
+        validationDatasetFilename = arguments.validationSet;
+        //check validation file for error
+
+        if (!(new File(validationDatasetFilename).exists())){
+            throw new IOException("Validation file not found! "+validationDatasetFilename);
+        }
         //Do training, and then generate and print samples from network
         int miniBatchNumber = 0;
         boolean init = true;
