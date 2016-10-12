1bb8cee19259a45b6f3326adff29d49185f4e136
Fabien Campagne <fac2003@campagnelab.org>
2016-10-12 16:21:51 -0400

    Optimize the readindex set implementation to save some time when mapping to features.

--- model-utils/src/main/java/org/campagnelab/dl/model/utils/mappers/ReadIndexWithCounts.java
+++ model-utils/src/main/java/org/campagnelab/dl/model/utils/mappers/ReadIndexWithCounts.java
@@ -1,6 +1,8 @@
 package org.campagnelab.dl.model.utils.mappers;
 
+import it.unimi.dsi.fastutil.ints.IntAVLTreeSet;
 import it.unimi.dsi.fastutil.ints.IntArraySet;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.List;
  * Created by fac2003 on 6/3/16.
  */
 public class ReadIndexWithCounts extends GenotypeCount {
-    private IntSet readIndices = new IntArraySet();
+    private IntSet readIndices = new IntOpenHashSet();
 
     public ReadIndexWithCounts() {
     }
