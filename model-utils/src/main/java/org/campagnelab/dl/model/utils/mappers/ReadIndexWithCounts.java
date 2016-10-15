fea6b36b709e59c970c930d92e627796930aa681
Fabien Campagne <fac2003@campagnelab.org>
2016-10-15 10:43:46 -0400

    Fix bug discovered in 0.6.0 branch: ReadIndexWithCounts: set must be cleared before adding elements to it.

--- model-utils/src/main/java/org/campagnelab/dl/model/utils/mappers/ReadIndexWithCounts.java
+++ model-utils/src/main/java/org/campagnelab/dl/model/utils/mappers/ReadIndexWithCounts.java
@@ -1,6 +1,7 @@
 package org.campagnelab.dl.model.utils.mappers;
 
 import it.unimi.dsi.fastutil.ints.IntArraySet;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 
 import java.util.List;
@@ -9,14 +10,13 @@ import java.util.List;
  * Created by fac2003 on 6/3/16.
  */
 public class ReadIndexWithCounts extends GenotypeCount {
-    private IntSet readIndices = new IntArraySet();
+    private IntSet readIndices = new IntOpenHashSet();
 
     public ReadIndexWithCounts() {
     }
 
-    public void set(List<Integer>
-            readIndicesForwardStrandList, List<Integer> readIndicesReverseStrandList) {
-
+    public void set(List<Integer> readIndicesForwardStrandList, List<Integer> readIndicesReverseStrandList) {
+        readIndices.clear();
         readIndices.addAll(readIndicesForwardStrandList);
         readIndices.addAll(readIndicesReverseStrandList);
     }
