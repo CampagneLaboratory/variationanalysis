cff83818997edd8c654bddf6609f5b9c96ccec0d
Fabien Campagne <fac2003@campagnelab.org>
2016-10-21 18:42:18 -0400

    Activate the remaining density mappers. Remi, please have a look at this code and check the logic.

--- model-utils/src/main/java/org/campagnelab/dl/model/utils/mappers/FeatureMapperV19.java
+++ model-utils/src/main/java/org/campagnelab/dl/model/utils/mappers/FeatureMapperV19.java
@@ -1,16 +1,11 @@
 package org.campagnelab.dl.model.utils.mappers;
 
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.campagnelab.dl.model.utils.ConfigurableFeatureMapper;
 import org.campagnelab.dl.model.utils.mappers.functional.TraversalHelper;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
 import org.campagnelab.goby.baseinfo.SequenceBaseInformationReader;
-import org.campagnelab.goby.baseinfo.StatAccumulatorNumVariationsInRead;
-import org.datavec.api.records.reader.RecordReader;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -26,11 +21,35 @@ public class FeatureMapperV19 extends NamingConcatFeatureMapper implements Confi
      */
     public void configure(SequenceBaseInformationReader reader) {
         Properties sbiProperties = reader.getProperties();
-        if (!sbiProperties.containsKey("stats.numVariationsInRead.min") || !sbiProperties.containsKey("stats.numVariationsInRead.max")) {
+        if (!propertiesPresent(sbiProperties, "stats.numVariationsInRead")) {
             throw new UnsupportedOperationException("The sbip file does not contain the statistics for numVariationsInRead (stats.numVariationsInRead.min and stats.numVariationsInRead.max)");
         }
-        float minNumVariationsInRead = Float.parseFloat(sbiProperties.getProperty("stats.numVariationsInRead.min"));
-        float maxNumVariationsInRead = Float.parseFloat(sbiProperties.getProperty("stats.numVariationsInRead.max"));
+        float minNumVariationsInRead = getMin(sbiProperties, "stats.numVariationsInRead");
+        float maxNumVariationsInRead = getMax(sbiProperties, "stats.numVariationsInRead");
+
+        if (!propertiesPresent(sbiProperties, "stats.readMappingQuality.forward")) {
+            throw new UnsupportedOperationException("The sbip file does not contain the statistics for stats.baseQuality.forward");
+        }
+        float minMappingQualityForward = getMin(sbiProperties, "stats.readMappingQuality.forward");
+        float maxMappingQualityForward = getMax(sbiProperties, "stats.readMappingQuality.forward");
+
+        if (!propertiesPresent(sbiProperties, "stats.readMappingQuality.reverse")) {
+            throw new UnsupportedOperationException("The sbip file does not contain the statistics for stats.baseQuality.forward");
+        }
+        float minMappingQualityReverse = getMin(sbiProperties, "stats.readMappingQuality.reverse");
+        float maxMappingQualityReverse = getMax(sbiProperties, "stats.readMappingQuality.reverse");
+
+        if (!propertiesPresent(sbiProperties, "stats.baseQuality.forward")) {
+            throw new UnsupportedOperationException("The sbip file does not contain the statistics for stats.baseQuality.forward");
+        }
+        float minBaseQualityForward = getMin(sbiProperties, "stats.baseQuality.forward");
+        float maxBaseQualityForward = getMax(sbiProperties, "stats.baseQuality.forward");
+
+        if (!propertiesPresent(sbiProperties, "stats.baseQuality.reverse")) {
+            throw new UnsupportedOperationException("The sbip file does not contain the statistics for stats.baseQuality.forward");
+        }
+        float minBaseQualityReverse = getMin(sbiProperties, "stats.baseQuality.reverse");
+        float maxBaseQualityReverse = getMax(sbiProperties, "stats.baseQuality.reverse");
 
         delegate = new NamingConcatFeatureMapper(new SimpleFeatureCalculator(true),
                 new IndelFeatures(),
@@ -38,10 +57,28 @@ public class FeatureMapperV19 extends NamingConcatFeatureMapper implements Confi
                 new FractionDifferences4(),
                 new MagnitudeFeatures2(),
                 new DensityMapper("numVariationsInRead", 10, minNumVariationsInRead, maxNumVariationsInRead, baseInformationOrBuilder ->
-                        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getNumVariationsInReadsList)));
-       // new DensityMapper("mappingQuality", 25, minNumVariationsInRead, maxNumVariationsInRead, baseInformationOrBuilder ->
-        //        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getMap)));
+                        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getNumVariationsInReadsList)),
+                new DensityMapper("mappingQualityForward", 25, minMappingQualityForward, maxMappingQualityForward, baseInformationOrBuilder ->
+                        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getReadMappingQualityForwardStrandList)),
+                new DensityMapper("mappingQualityReverse", 25, minMappingQualityReverse, maxMappingQualityReverse, baseInformationOrBuilder ->
+                        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getReadMappingQualityReverseStrandList)),
+                new DensityMapper("baseQualityForward", 10, minBaseQualityForward, maxBaseQualityForward, baseInformationOrBuilder ->
+                        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getQualityScoresForwardStrandList)),
+                new DensityMapper("baseQualityReverse", 10, minBaseQualityReverse, maxBaseQualityReverse, baseInformationOrBuilder ->
+                        TraversalHelper.forAllSampleCounts(baseInformationOrBuilder, BaseInformationRecords.CountInfo::getQualityScoresReverseStrandList)));
 
+    }
+
+    private float getMin(Properties sbiProperties, String propertyName) {
+        return Float.parseFloat(sbiProperties.getProperty(propertyName + ".min"));
+    }
+
+    private float getMax(Properties sbiProperties, String propertyName) {
+        return Float.parseFloat(sbiProperties.getProperty(propertyName + ".max"));
+    }
+
+    private boolean propertiesPresent(Properties sbiProperties, String s) {
+        return sbiProperties.containsKey(s + ".min") || !sbiProperties.containsKey(s + ".max");
     }
 
 
