0512f491459bb956932fc8581df441002b82eba8
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2016-11-14 16:24:20 -0500

    mutationdirection test finished and passing. prints all tests so that the range of possible mutations is verifiable. At the moment it seems that not only are wrong mutations not generated, but all possible correct mutations are generated.

--- model-training/src/test/java/org/campagnelab/dl/varanalysis/intermediaries/MutationDirectionTest.java
+++ model-training/src/test/java/org/campagnelab/dl/varanalysis/intermediaries/MutationDirectionTest.java
@@ -5,6 +5,7 @@ import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
 import org.junit.Test;
 
 import java.io.StringWriter;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -19,17 +20,18 @@ public class MutationDirectionTest {
     public void mutateTest() throws Exception {
         FirstSimulationStrategy strategy = new FirstSimulationStrategy(1,1,0.1,1);
         final int mostCount = 100;
-        final int secondMostCount = 11;
+        final int secondMostCount = 12;
         //homozygous first:
         for (int numGenos = 5; numGenos < 8; numGenos++){
             for (int maxIndex = 0; maxIndex < numGenos; maxIndex++){
                 if (maxIndex == 4){
-                    break;
+                    continue;
                 }
                 int[] counts = new int[numGenos];
                 counts[maxIndex] = mostCount;
-                for (int trial = 0; trial < 10; trial++){
+                for (int trial = 0; trial < 20; trial++){
                     FirstSimulationStrategy.mutationDirection dir = strategy.dirFromCounts(counts);
+                    System.out.println(Arrays.toString(counts)+ " " + dir.oldBase + " -> "+ dir.newBase);
                     assert(dir.newBase !=dir.oldBase);
                     assert(dir.newBase !=maxIndex);
                     assert(dir.newBase != 4);
@@ -37,21 +39,23 @@ public class MutationDirectionTest {
                 }
             }
         }
+        System.out.println("\nHeterozygous:");
         //now heterozygous
         for (int numGenos = 5; numGenos < 8; numGenos++){
             for (int maxIndex = 0; maxIndex < numGenos; maxIndex++){
                 if (maxIndex == 4){
-                    break;
+                    continue;
                 }
                 for (int secondMostIndex = 0; secondMostIndex < numGenos; secondMostIndex++){
                     if (secondMostIndex == maxIndex || secondMostIndex == 4){
-                        break;
+                        continue;
                     }
                     int[] counts = new int[numGenos];
                     counts[maxIndex] = mostCount;
                     counts[secondMostIndex] = secondMostCount;
-                    for (int trial = 0; trial < 10; trial++){
+                    for (int trial = 0; trial < 20; trial++){
                         FirstSimulationStrategy.mutationDirection dir = strategy.dirFromCounts(counts);
+                        System.out.println(Arrays.toString(counts)+ " " + dir.oldBase + " -> "+ dir.newBase);
                         assert(dir.newBase != dir.oldBase);
                         assert(dir.newBase != maxIndex);
                         assert(dir.newBase != 4);
