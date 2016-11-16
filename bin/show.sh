ac78a8fda4f84b31d4a005169fde75b6ae740395
Fabien Campagne <fac2003@campagnelab.org>
2016-11-16 14:26:48 -0500

    Upgrade bin wrappers after refactoring. Switch train-somatic.sh to use TrainModelS.

--- bin/show.sh
+++ bin/show.sh
@@ -2,4 +2,4 @@
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
 java -Xmx${memory_requirement} -cp ${DLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG}   \
-    org.campagnelab.dl.varanalysis.tools.Show ${other_parameters}
+    org.campagnelab.dl.somatic.tools.Show ${other_parameters}
