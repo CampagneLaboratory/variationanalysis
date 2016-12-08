6a031451a1c28e92c3dbeec31eea28d5e37b772d
Fabien Campagne <fac2003@campagnelab.org>
2016-12-08 12:04:24 -0500

    Fix typo.

--- bin/search-hyper-params.sh
+++ bin/search-hyper-params.sh
@@ -45,11 +45,13 @@ cat << EOF | cat>gpu.txt
 3
 EOF
 
+NUM_GPUS=`wc -l gpus.txt|cut -d " " -f 1`
+
 num_executions=${memory_requirement}
 
 parallel echo `cat main-command.txt` --regularization-rate :::: reg.txt :::  --random-seed :::: seed.txt ::: --learning-rate :::: learn.txt ::: --dropout-rate :::: drop.txt ::: --early-stopping-num-epochs ::: 0   >commands.txt
 shuf commands.txt  |head -${num_executions} >commands-head-${num_executions}
 chmod +x commands-head-${num_executions}
 cat ./commands-head-${num_executions} |parallel --xapply ::: echo  :::: - ::: --gpu-device :::: gpu.txt  >all-commands.txt
-cat all-commands.txt |parallel -j8 --progress
+cat all-commands.txt |parallel -j${GPU_NUM} --progress
 sort -n -k 2 model-conditions.txt|tail
