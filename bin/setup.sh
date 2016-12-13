07ed1f4701a267d5df6eaffbc843a9827fa04dba
Manuele Simi <manuele.simi@campagnelab.org>
2016-12-13 16:39:55 -0500

    Request cuda platform in train-somatic* wrappers. Default to native if cuda is not available. Default to native for the other wrappers.

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,5 +9,14 @@ if [[ $OSTYPE == "cygwin" ]]; then
 fi
 export VERSION=`cat ${DISTRIBUTION_DIR}/VERSION.txt`
 export DLVA_HOME=${DISTRIBUTION_DIR}
-export DLVA_JAR=${DLVA_HOME}/gpus/target/gpus-${VERSION}.jar:${DLVA_HOME}/somatic/target/somatic-${VERSION}-bin.jar
+if [ -n "${FORCE_PLATFORM+set}" ]; then
+    LATEST_PLATFORM=`cat {DISTRIBUTION_DIR}/PROFILE.txt`
+    if [ "$FORCE_PLATFORM" == "$LATEST_PLATFORM" ]; then
+           export DLVA_JAR=${DLVA_HOME}/gpus/target/gpus-${VERSION}.jar:${DLVA_HOME}/somatic/target/somatic-${VERSION}-bin-${FORCE_PLATFORM}.jar
+    fi
+fi
+
+if [ -n "${DLVA_JAR+set}" ]; then
+    export DLVA_JAR=${DLVA_HOME}/gpus/target/gpus-${VERSION}.jar:${DLVA_HOME}/somatic/target/somatic-${VERSION}-bin-native.jar
+fi
 export SLF4J_CONFIG=${DLVA_HOME}/config/logback.xml
