56932fff3e0c4e7e170332117cb153472228b416
Manuele Simi <manuelesimi@Manueles-MacBook-Pro.local>
2016-12-13 21:56:50 -0500

    Add missing parenthesis in setup.sh.

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,7 +12,7 @@ export DLVA_HOME=${DISTRIBUTION_DIR}
 EXECUTION_PLATFORM=`tail -1 ${DISTRIBUTION_DIR}/PROFILE.txt`
 
 if [ -n "${FORCE_PLATFORM+set}" ]; then
-    EXECUTION_PLATFORM="${FORCE_PLATFORM"
+    EXECUTION_PLATFORM="${FORCE_PLATFORM}"
 fi
 export DLVA_JAR=${DLVA_HOME}/gpus/target/gpus-${VERSION}.jar:${DLVA_HOME}/somatic/target/somatic-${VERSION}-bin-${EXECUTION_PLATFORM}.jar
 export SLF4J_CONFIG=${DLVA_HOME}/config/logback.xml
