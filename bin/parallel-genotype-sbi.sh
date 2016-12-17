5e359d0d2be52c296e7761af160fd1f814f8e304
Fabien Campagne <fac2003@campagnelab.org>
2016-12-16 19:12:12 -0500

    Enable realignment in parallel genotype script.

--- bin/parallel-genotype-sbi.sh
+++ bin/parallel-genotype-sbi.sh
@@ -3,7 +3,6 @@
 
 ALIGNMENTS="$*"
 
-
 if [ -z "${SBI_GENOME+set}" ]; then
     SBI_GENOME="/data/genomes/Homo_sapiens.ucsc.hg19"
     echo "SBI_GENOME set to ${SBI_GENOME}. Change the variable to influence the genome used (must be indexed with goby build-sequence-cache)."
@@ -16,11 +15,11 @@ echo "variables: ${SBI_GENOME} ${SBI_NUM_THREADS}"
 
 goby ${memory_requirement} suggest-position-slices ${ALIGNMENTS} --number-of-slices 200 -o slices.tsv
 grep -v targetIdStart slices.tsv >slices
-echo " discover-sequence-variants -n 1 -t 1 --genome  ${SBI_GENOME} --format  SEQUENCE_BASE_INFORMATION  ${ALIGNMENTS} --max-coverage-per-site 10000" >command.txt
+echo " discover-sequence-variants -n 1 -t 1 --genome  ${SBI_GENOME} --format  SEQUENCE_BASE_INFORMATION  ${ALIGNMENTS} \
+    --call-indels  true --processor realign_near_indels \
+    --max-coverage-per-site 10000" >command.txt
 
 cut -f3,6 slices  | awk 'BEGIN{count=1} {print "-s "$1" -e " $2" -o out-part-"(count++)}' >boundaries
 parallel -j2 --plus  --progress goby ${memory_requirement}  `cat command.txt`  :::: boundaries
-
-
 
 concat.sh ${memory_requirement} out-part-*.sbi -o out-concat.sbi
