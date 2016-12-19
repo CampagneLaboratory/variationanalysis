5366280c7afe29e6a3c19d5351d65b96c9afc33f
Fabien Campagne <fac2003@campagnelab.org>
2016-12-19 18:10:34 -0500

    Support multiple alleles in add-true-genotypes, not just diploid.

--- genotype/src/main/java/org/campagnelab/dl/genotype/tools/AddTrueGenotypes.java
+++ genotype/src/main/java/org/campagnelab/dl/genotype/tools/AddTrueGenotypes.java
@@ -4,7 +4,10 @@ package org.campagnelab.dl.genotype.tools;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.io.BinIO;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.ObjectArraySet;
+import it.unimi.dsi.fastutil.objects.ObjectSet;
 import it.unimi.dsi.logging.ProgressLogger;
+import it.unimi.dsi.util.XorShift1024StarRandom;
 import org.campagnelab.dl.framework.tools.arguments.AbstractTool;
 import org.campagnelab.dl.somatic.storage.RecordReader;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
@@ -13,6 +16,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Random;
 
 /**
  * The addcalls object uses a map to create a new protobuf file with genotype calls.
@@ -38,6 +43,8 @@ public class AddTrueGenotypes extends AbstractTool<AddTrueGenotypesArguments> {
     //only supports genotypes encoded with a bar (|) delimiter
     public void execute() {
         int sampleIndex = args().sampleIndex;
+
+      // Random random=  new XorShift1024StarRandom();
         try {
             Object2ObjectMap<String,Int2ObjectMap<String>> chMap = (Object2ObjectMap<String,Int2ObjectMap<String>>)BinIO.loadObject(args().genotypeMap);
             RecordReader source = new RecordReader(args().inputFile);
@@ -49,27 +56,19 @@ public class AddTrueGenotypes extends AbstractTool<AddTrueGenotypesArguments> {
             int recordsLabeled = 0;
             recordLogger.start();
             for (BaseInformationRecords.BaseInformation rec : source) {
-                //first, we skip examples where all reads match the reference
-                boolean skip = true;
-                for (BaseInformationRecords.CountInfo count : rec.getSamples(sampleIndex).getCountsList()){
-                    if (!count.getMatchesReference() && (count.getGenotypeCountForwardStrand() + count.getGenotypeCountReverseStrand()) != 0) {
-                        skip = false;
-                        break;
-                    }
-                }
-                if (skip) {
-                    continue;
-                }
+
                 BaseInformationRecords.BaseInformation.Builder buildRec = rec.toBuilder();
                 int position = buildRec.getPosition();
                 String chrom = buildRec.getReferenceId();
                 String[] genotypes = new String[2];
                 String trueGenotype;
                 boolean isVariant = false;
+                ObjectSet<String> trueAlleles=new ObjectArraySet<>();
                 try {
                     // The map contains Goby positions (zero-based).
                     trueGenotype = chMap.get(chrom).get(position);
-                    genotypes = trueGenotype.split("|");
+                    genotypes = trueGenotype.split("[|/]");
+                    Collections.addAll(trueAlleles,genotypes);
                     isVariant = true;
                 } catch (NullPointerException e) {
                     String referenceBase = buildRec.getReferenceBase();
@@ -81,9 +80,12 @@ public class AddTrueGenotypes extends AbstractTool<AddTrueGenotypesArguments> {
                 BaseInformationRecords.SampleInfo.Builder buildSample = buildRec.getSamples(sampleIndex).toBuilder();
                 for (int i = 0; i < buildSample.getCountsCount(); i++){
                     BaseInformationRecords.CountInfo.Builder count = buildSample.getCounts(i).toBuilder();
-                    boolean isCalled = (count.getToSequence().equals(genotypes[0])||count.getToSequence().equals(genotypes[1]));
+                    boolean isCalled =  trueAlleles.contains(count.getToSequence());
                     count.setIsCalled(isCalled);
                     buildSample.setCounts(i,count);
+                    /*if (isCalled) {
+                        System.out.printf("true=%s count=%s%n",trueGenotype,buildSample.getFormattedCounts());
+                    }*/
                 }
                 buildSample.setIsVariant(isVariant);
                 buildRec.setSamples(sampleIndex,buildSample.build());
