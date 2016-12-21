ee6b8835cdb663c32c1034e3487f1b9d696d8913
Fabien Campagne <fac2003@campagnelab.org>
2016-12-21 14:13:02 -0500

    Clear cache before run.

--- bin/iterate-genotype.sh
+++ bin/iterate-genotype.sh
@@ -25,11 +25,15 @@ if [ ! -e "${DATASET}test.sbi" ]; then
            exit 1;
 fi
 echo "Iteration for FEATURE_MAPPER=${FEATURE_MAPPER}"
+
 export FORCE_PLATFORM=native
+rm ${DATASET}train*.cf ${DATASET}${VAL_SUFFIX}*cf
 train-genotype.sh 10g -t ${DATASET}train.sbi -v ${DATASET}${VAL_SUFFIX}.sbi \
  --mini-batch-size 2048 -r 5 --feature-mapper ${FEATURE_MAPPER} --build-cache-then-stop
+
 export FORCE_PLATFORM=cuda
 train-genotype.sh 10g -t ${DATASET}train.sbi -v ${DATASET}${VAL_SUFFIX}.sbi \
   --mini-batch-size 2048 -r 5 --feature-mapper ${FEATURE_MAPPER} --early-stopping-num-epochs 1
+
 MODEL_TIMESTAMP=`ls -1tr models|tail -1`
 predict-genotypes.sh 10g -m models/${MODEL_TIMESTAMP} -l bestscore -f -i ${DATASET}test.sbi
