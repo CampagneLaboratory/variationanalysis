d906aab3a290acf641eb7480d52d9fbd8baf2b32
Fabien Campagne <fac2003@campagnelab.org>
2016-12-21 14:08:38 -0500

    Fix suffixes.

--- bin/iterate-genotype.sh
+++ bin/iterate-genotype.sh
@@ -12,24 +12,24 @@ if [ -z "${VAL_SUFFIX+set}" ]; then
     VAL_SUFFIX="validation"
     echo "VAL_SUFFIX set to ${VAL_SUFFIX}. Change the variable to switch the validation suffix."
 fi
-if [ -n -e ${DATASET}${VAL_SUFFIX}.sbi ]; then
+if [ ! -e "${DATASET}${VAL_SUFFIX}.sbi" ]; then
     echo "The validation set was not found: ${DATASET}${VAL_SUFFIX}.sbi  "
        exit 1;
 fi
-if [ -n -e ${DATASET}train.sbi ]; then
+if [ ! -e "${DATASET}train.sbi" ]; then
     echo "The training set was not found: ${DATASET}train.sbi  "
        exit 1;
 fi
-if [ -n -e ${DATASET}test.sbi ]; then
+if [ ! -e "${DATASET}test.sbi" ]; then
         echo "The test set was not found: ${DATASET}test.sbi  "
            exit 1;
 fi
 echo "Iteration for FEATURE_MAPPER=${FEATURE_MAPPER}"
 export FORCE_PLATFORM=native
-train-genotype.sh 10g -t ${DATASET}training.sbi -v ${DATASET}validation.sbi \
+train-genotype.sh 10g -t ${DATASET}train.sbi -v ${DATASET}${VAL_SUFFIX}.sbi \
  --mini-batch-size 2048 -r 5 --feature-mapper ${FEATURE_MAPPER} --build-cache-then-stop
 export FORCE_PLATFORM=cuda
-train-genotype.sh 10g -t ${DATASET}training.sbi -v ${DATASET}${VAL_SUFFIX}.sbi \
+train-genotype.sh 10g -t ${DATASET}train.sbi -v ${DATASET}${VAL_SUFFIX}.sbi \
   --mini-batch-size 2048 -r 5 --feature-mapper ${FEATURE_MAPPER} --early-stopping-num-epochs 1
 MODEL_TIMESTAMP=`ls -1tr models|tail -1`
 predict-genotypes.sh 10g -m models/${MODEL_TIMESTAMP} -l bestscore -f -i ${DATASET}test.sbi
