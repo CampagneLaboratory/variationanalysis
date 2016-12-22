41dbf7c4bd867b476f112f96bf3103ea78b2337d
Fabien Campagne <fac2003@campagnelab.org>
2016-12-22 09:44:12 -0500

    V10 is now training and reporting F1.

--- genotype/src/main/java/org/campagnelab/dl/genotype/learning/domains/predictions/CombinedOutputLayerInterpreter.java
+++ genotype/src/main/java/org/campagnelab/dl/genotype/learning/domains/predictions/CombinedOutputLayerInterpreter.java
@@ -2,9 +2,9 @@ package org.campagnelab.dl.genotype.learning.domains.predictions;
 
 import org.campagnelab.dl.framework.domains.prediction.PredictionInterpreter;
 import org.campagnelab.dl.genotype.mappers.CombinedLabelsMapper;
-import org.campagnelab.dl.genotype.predictions.CombinedGenotypePrediction;
 import org.campagnelab.dl.genotype.predictions.CombinedOutputLayerPrediction;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
+import org.jetbrains.annotations.NotNull;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
 /**
@@ -14,16 +14,27 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 public class CombinedOutputLayerInterpreter extends SortingCountInterpreter<CombinedOutputLayerPrediction>
         implements PredictionInterpreter<BaseInformationRecords.BaseInformation, CombinedOutputLayerPrediction> {
 
+    private static final int MAX_GENOTYPES = 2;
+    String[] toSequences;
 
     public CombinedOutputLayerInterpreter() {
         super(true);
+        // the default toSequences are integers, because we don't have access to exact genotypes (we have no record),
+        // but integers are sufficient to calculate performance stats.
+        toSequences = new String[2];
+        for (int genotypeIndex = 0; genotypeIndex < MAX_GENOTYPES; genotypeIndex++) {
+            toSequences[genotypeIndex] = Integer.toString(genotypeIndex);
+        }
     }
 
     double probability;
 
     @Override
     public CombinedOutputLayerPrediction interpret(INDArray trueLabels, INDArray output, int predictionIndex) {
-        throw new RuntimeException("a wrong interpret method was called on the homozygous interpeter");
+        CombinedOutputLayerPrediction p = new CombinedOutputLayerPrediction();
+        p.trueGenotype = reconstructGenotype(trueLabels);
+        p.predictedGenotype = reconstructGenotype(output);
+        return p;
     }
 
     @Override
@@ -37,6 +48,14 @@ public class CombinedOutputLayerInterpreter extends SortingCountInterpreter<Comb
 
     public String getPrediction(BaseInformationRecords.BaseInformation currentRecord, INDArray output) {
         sortedCountRecord = sort(currentRecord);
+        for (int genotypeIndex = 0; genotypeIndex < MAX_GENOTYPES; genotypeIndex++) {
+            toSequences[genotypeIndex] = sortedCountRecord.getSamples(0).getCounts(genotypeIndex).getToSequence();
+        }
+        return reconstructGenotype(output);
+    }
+
+    @NotNull
+    private String reconstructGenotype(INDArray output) {
         probability = -1;
         int maxIndex = -1;
         for (int i = 0; i < CombinedLabelsMapper.NUM_LABELS; i++) {
@@ -46,17 +65,16 @@ public class CombinedOutputLayerInterpreter extends SortingCountInterpreter<Comb
                 probability = outputDouble;
             }
         }
-
         switch (maxIndex) {
             case 0:
-                String homozygAllele = sortedCountRecord.getSamples(0).getCounts(0).getToSequence();
+                String homozygAllele = toSequences[0];
                 return homozygAllele + "/" + homozygAllele;
             case 1:
-                String firstAllele = sortedCountRecord.getSamples(0).getCounts(0).getToSequence();
-                String secondAllele = sortedCountRecord.getSamples(0).getCounts(1).getToSequence();
+                String firstAllele = toSequences[0];
+                String secondAllele = toSequences[1];
                 return firstAllele + "/" + secondAllele;
             case 2:
-                homozygAllele = sortedCountRecord.getSamples(0).getCounts(1).getToSequence();
+                homozygAllele = toSequences[1];
                 return homozygAllele + "/" + homozygAllele;
             default:
                 // other site predicted, mark as wrong
