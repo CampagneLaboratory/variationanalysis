2954b5b6a7c64ecb8915864678f9d815f37dd6e2
Fabien Campagne <fac2003@campagnelab.org>
2017-01-02 09:28:26 -0500

    Now use Layer assembler in NumDistinctAlleleAssembler. Remove 8Layer version of this class now that we can parameterize number of layers from the command line.

--- genotype/src/main/java/org/campagnelab/dl/genotype/learning/architecture/graphs/NumDistinctAlleleAssembler.java
+++ genotype/src/main/java/org/campagnelab/dl/genotype/learning/architecture/graphs/NumDistinctAlleleAssembler.java
@@ -4,6 +4,7 @@ import org.campagnelab.dl.framework.architecture.graphs.ComputationGraphAssemble
 import org.campagnelab.dl.framework.domains.DomainDescriptor;
 import org.campagnelab.dl.framework.models.ModelPropertiesHelper;
 import org.campagnelab.dl.framework.tools.TrainingArguments;
+import org.campagnelab.dl.genotype.learning.GenotypeTrainingArguments;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
 import org.deeplearning4j.nn.conf.LearningRatePolicy;
@@ -24,95 +25,51 @@ public class NumDistinctAlleleAssembler implements ComputationGraphAssembler {
     private TrainingArguments arguments;
     private int numInputs;
     private String[] outputNames = new String[]{"numDistinctAlleles", "A", "T", "C", "G", "N", "I1", "I2", "I3", "I4", "I5", "metaData"};
+    private int numLayers;
 
     private TrainingArguments args() {
         return arguments;
     }
 
-    ;
+   FeedForwardDenseLayerAssembler layerAssembler;
 
     @Override
     public void setArguments(TrainingArguments arguments) {
         this.arguments = arguments;
+        this.numLayers = ((GenotypeTrainingArguments) arguments).numLayers;
+        layerAssembler=new FeedForwardDenseLayerAssembler(arguments);
     }
 
     @Override
     public ComputationGraph createComputationalGraph(DomainDescriptor domainDescriptor) {
+        layerAssembler.setLearningRatePolicy(learningRatePolicy);
         int numInputs = domainDescriptor.getNumInputs("input")[0];
         int numHiddenNodes = domainDescriptor.getNumHiddenNodes("firstDense");
-
+        ComputationGraphConfiguration.GraphBuilder build = layerAssembler.assemble(numInputs, numHiddenNodes, numLayers);
+        int numIn = layerAssembler.getNumOutputs();
         WeightInit WEIGHT_INIT = WeightInit.XAVIER;
-        learningRatePolicy = LearningRatePolicy.Poly;
-        float reduction = 1f;
-        int minimum = (int) (numHiddenNodes * Math.pow(reduction, 4));
-        assert minimum > 2 : "Too much reduction, not enough outputs: ";
-        ComputationGraphConfiguration confBuilder = null;
-        NeuralNetConfiguration.Builder graphBuilder = new NeuralNetConfiguration.Builder()
-                .seed(args().seed)
-                .iterations(1)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .learningRate(args().learningRate)
-                .updater(Updater.ADAGRAD);
-        if (args().regularizationRate != null) {
-            graphBuilder.l2(args().regularizationRate);
-        }
-        if (args().dropoutRate != null) {
-            graphBuilder.dropOut(args().dropoutRate);
-            graphBuilder.setUseDropConnect(true);
-        }
-        NeuralNetConfiguration.Builder graphConfiguration = graphBuilder.lrPolicyDecayRate(0.5)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
-                .learningRate(args().learningRate)
-                .seed(args().seed);
-        if (args().regularizationRate != null) {
-            graphConfiguration.regularization(args().regularizationRate != null);
-        }
-        if (args().dropoutRate != null) {
-            graphConfiguration.dropOut(args().dropoutRate);
-            graphConfiguration.setUseDropConnect(true);
-        }
-        ComputationGraphConfiguration.GraphBuilder build = graphConfiguration
-                .weightInit(WeightInit.XAVIER).graphBuilder().addInputs("input")
-                .addLayer("dense1", new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
-                        .weightInit(WEIGHT_INIT)
-                        .activation("relu").learningRateDecayPolicy(learningRatePolicy)
-                        .build(), "input")
-                .addLayer("dense2", new DenseLayer.Builder().nIn(numHiddenNodes).nOut((int) (numHiddenNodes * reduction))
-                        .weightInit(WEIGHT_INIT)
-                        .activation("relu").learningRateDecayPolicy(learningRatePolicy)
-                        .build(), "dense1")
-                .addLayer("dense3", new DenseLayer.Builder().nIn((int) (numHiddenNodes * reduction)).nOut((int) (numHiddenNodes * Math.pow(reduction, 2)))
-                        .weightInit(WEIGHT_INIT).learningRateDecayPolicy(learningRatePolicy)
-                        .activation("relu")
-                        .build(), "dense2")
-                .addLayer("dense4", new DenseLayer.Builder().nIn((int) (numHiddenNodes * Math.pow(reduction, 2))).nOut((int) (numHiddenNodes * Math.pow(reduction, 3)))
-                        .weightInit(WEIGHT_INIT).learningRateDecayPolicy(learningRatePolicy)
-                        .activation("relu")
-                        .build(), "dense3")
-                .addLayer("dense5", new DenseLayer.Builder().nIn((int) (numHiddenNodes * Math.pow(reduction, 3))).nOut((int) (numHiddenNodes * Math.pow(reduction, 4)))
-                        .weightInit(WEIGHT_INIT).learningRateDecayPolicy(learningRatePolicy)
-                        .activation("relu")
-                        .build(), "dense4");
+        String lastDenseLayerName = layerAssembler.lastLayerName();
+
         build.addLayer("numDistinctAlleles", new OutputLayer.Builder(
                 domainDescriptor.getOutputLoss("numDistinctAlleles"))
                 .weightInit(WEIGHT_INIT)
                 .activation("softmax").weightInit(WEIGHT_INIT).learningRateDecayPolicy(learningRatePolicy)
-                .nIn((int) (numHiddenNodes * Math.pow(reduction, 4)))
-                .nOut(domainDescriptor.getNumOutputs("numDistinctAlleles")[0]).build(), "dense5").addInputs();
+                .nIn(numIn)
+                .nOut(domainDescriptor.getNumOutputs("numDistinctAlleles")[0]).build(), lastDenseLayerName).addInputs();
         build.addLayer("metaData", new OutputLayer.Builder(
                 domainDescriptor.getOutputLoss("metaData"))
                 .weightInit(WEIGHT_INIT)
                 .activation("softmax").weightInit(WEIGHT_INIT).learningRateDecayPolicy(learningRatePolicy)
-                .nIn((int) (numHiddenNodes * Math.pow(reduction, 4)))
+                .nIn(numIn)
                 .nOut(
                         domainDescriptor.getNumOutputs("metaData")[0]
-                ).build(), "dense5");
+                ).build(), lastDenseLayerName);
         for (int i = 1; i < outputNames.length; i++) {
             build.addLayer(outputNames[i], new OutputLayer.Builder(
                     domainDescriptor.getOutputLoss(outputNames[i]))
                     .weightInit(WEIGHT_INIT)
                     .activation("softmax").weightInit(WEIGHT_INIT).learningRateDecayPolicy(learningRatePolicy)
-                    .nIn((int) (numHiddenNodes * Math.pow(reduction, 4))).nOut(domainDescriptor.getNumOutputs(outputNames[i])[0]).build(), "dense5");
+                    .nIn(numIn).nOut(domainDescriptor.getNumOutputs(outputNames[i])[0]).build(), lastDenseLayerName);
         }
         ComputationGraphConfiguration conf = build
                 .setOutputs(outputNames)
