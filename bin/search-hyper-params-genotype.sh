9bfaf89831dbdbb33f24ef1e1de17c9f03a87e01
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2017-01-06 18:06:05 -0500

    search hyper uses random args.

--- bin/search-hyper-params-genotype.sh
+++ bin/search-hyper-params-genotype.sh
@@ -2,47 +2,39 @@
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 assertParallelInstalled
 
-cat << EOF | cat> learn.txt
-5
-EOF
+cat << EOF | cat> config.txt
+--regularization-rate
+log-uniform
+1E-10
+1E-1
 
-cat << EOF | cat> drop.txt
+--random-seed
+categorical
+5
+
+--learning-rate
+categorical
+5
+
+--dropout-rate
+uniform
 0.5
 1
-EOF
 
+--early-stopping-num-epochs
+categorical
+1
 
-cat << EOF | cat> reg.txt
-0
+--variant-loss-weight
+log-uniform
 1E-10
-1E-7
-1E-5
-1E-4
-1E-2
-EOF
-
-cat << EOF | cat>seed.txt
-443
-EOF
-
-cat << EOF | cat>genomic-context-size.txt
-21
-31
-41
-51
-61
-EOF
-
-
-cat << EOF | cat>is-variant-loss-weight.txt
-0
-5
-10
-50
-100
 1000
-EOF
 
+--genomic-context-length
+int
+21
+41
+EOF
 
 cat << EOF | cat>gpu.txt
 0
@@ -51,30 +43,31 @@ cat << EOF | cat>gpu.txt
 3
 EOF
 
+
+
+
 echo $* >main-command.txt
 NUM_GPUS=`wc -l gpu.txt|cut -d " " -f 1`
 
 num_executions=${memory_requirement}
 
+. `dirname "${BASH_SOURCE[0]}"`/arg-generator.sh 1g --config config.txt --output gen-args.txt --num-commands ${num_executions}
+
 parallel echo `cat main-command.txt` --mini-batch-size 2048 \
   --build-cache-then-stop \
-  --genomic-context-length :::: genomic-context-size.txt ::: \
->build-cache-commands.txt
+  :::: gen-args.txt ::: \
+>build-cache-commands.txtbuil
 
 export FORCE_PLATFORM=native
 cat build-cache-commands.txt |parallel -j${NUM_GPUS} --progress
 
 unset FORCE_PLATFORM
 parallel echo `cat main-command.txt` --mini-batch-size 2048 \
-  --regularization-rate :::: reg.txt :::  \
-  --random-seed :::: seed.txt ::: --learning-rate :::: learn.txt ::: \
-  --dropout-rate :::: drop.txt ::: --early-stopping-num-epochs ::: 1 ::: \
-  --variant-loss-weight :::: is-variant-loss-weight.txt ::: \
-  --genomic-context-length :::: genomic-context-size.txt ::: \
+  :::: gen-args.txt \
 >commands.txt
 
 shuf commands.txt  |head -${num_executions} >commands-head-${num_executions}
 chmod +x commands-head-${num_executions}
-cat ./commands-head-${num_executions} |parallel --xapply ::: echo  :::: - ::: --gpu-device :::: gpu.txt  >all-commands.txt
+cat ./commands-head-${num_executions} |parallel --xapply echo :::: - ::: --gpu-device :::: gpu.txt  >all-commands.txt
 cat all-commands.txt |parallel -j${NUM_GPUS} --progress
 sort -n -k 2 model-conditions.txt|tail
