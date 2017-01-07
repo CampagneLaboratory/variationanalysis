99d5025c754d37c1666064ba72964b36134f8033
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2017-01-06 20:49:06 -0500

    update parallel bam to goby.

--- bin/parallel-bam-to-goby.sh
+++ bin/parallel-bam-to-goby.sh
@@ -24,8 +24,8 @@ if [ -z "${SBI_GENOME+set}" ]; then
     echo "SBI_GENOME set to ${SBI_GENOME}. Change the variable to influence the genome used (must be indexed with goby build-sequence-cache)."
 fi
 if [ -z "${FASTA_GENOME+set}" ]; then
-    SBI_GENOME="/data/genomes/Homo_sapiens.ucsc.hg19.fa"
-    echo "SBI_GENOME set to ${SBI_GENOME}. Change the variable to influence the fasta used."
+    FASTA_GENOME="/data/genomes/Homo_sapiens.ucsc.hg19.fa"
+    echo "FASTA_GENOME set to ${FASTA_GENOME}. Change the variable to influence the fasta used."
 fi
 if [ -z  "${SBI_NUM_THREADS+set}" ]; then
     SBI_NUM_THREADS="2"
@@ -33,25 +33,21 @@ if [ -z  "${SBI_NUM_THREADS+set}" ]; then
 fi
 echo "variables: ${SBI_GENOME} ${SBI_NUM_THREADS}"
 
-goby ${memory_requirement} suggest-position-slices ${ALIGNMENTS} --number-of-slices 60-o slices.tsv
-grep -v targetIdStart slices.tsv >slices
-
+goby ${memory_requirement} suggest-position-slices ${ALIGNMENTS} --number-of-slices 60 -o slices.tsv --restrict-per-chromosome
+rm -rf calmd-and-convert-commands.txt
 nLine=0
-tail -n +2 file.txt | while read line
+tail -n +2 slices.tsv | while read -r line
     do
-       sRef=`cut -f1 line`
-       sPos=`cut -f2 line`
-       ePos=`cut -f5 line`
-       samtools view -b ${ALIGNMENTS} ${sReg}:${sPos}-${ePos} > slice_${nLine}.bam
-       echo "samtools calmd $slice_{nLine}.bam ${FASTA_GENOME} > md_slice_${nLine}.bam ;\
+       sRef=`echo $line | cut -f1 -d ' '`
+       sPos=`echo $line | cut -f2 -d ' '`
+       ePos=`echo $line | cut -f5 -d ' '`
+       samtools view -b ${ALIGNMENTS} ${sRef}:${sPos}-${ePos} > slice_${nLine}.bam
+       echo "samtools calmd slice_${nLine}.bam ${FASTA_GENOME} > md_slice_${nLine}.bam ;\
         goby ${memory_requirement} concatenate-alignments --genome  ${SBI_GENOME}  md_slice_${nLine}.bam -o goby_slice_${nLine}"\
          >> calmd-and-convert-commands.txt
-       var=$((var+1))
-       fi
-    done
+       nLine=$((nLine+1))
 done
 
-for
-parallel -j${SBI_NUM_THREADS} -a --progress calmd-and-convert-commands.txt echo
+parallel --progress -j${SBI_NUM_THREADS} -a  calmd-and-convert-commands.txt echo
 
 goby ${memory_requirement} concatenate-alignments goby_slice_*.entries -o ${OUTPUT_BASENAME}
