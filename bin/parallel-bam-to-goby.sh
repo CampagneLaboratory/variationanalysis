b7b9532e8f4c6ca11c56c3dcb1b45b947f873799
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2017-01-06 19:18:03 -0500

    calmd added to bam to goby. requires fasta genome now. need working goby jar to test.

--- bin/parallel-bam-to-goby.sh
+++ bin/parallel-bam-to-goby.sh
@@ -23,17 +23,35 @@ if [ -z "${SBI_GENOME+set}" ]; then
     SBI_GENOME="/data/genomes/Homo_sapiens.ucsc.hg19"
     echo "SBI_GENOME set to ${SBI_GENOME}. Change the variable to influence the genome used (must be indexed with goby build-sequence-cache)."
 fi
+if [ -z "${FASTA_GENOME+set}" ]; then
+    SBI_GENOME="/data/genomes/Homo_sapiens.ucsc.hg19.fa"
+    echo "SBI_GENOME set to ${SBI_GENOME}. Change the variable to influence the fasta used."
+fi
 if [ -z  "${SBI_NUM_THREADS+set}" ]; then
     SBI_NUM_THREADS="2"
     echo "SBI_NUM_THREADS set to ${SBI_NUM_THREADS}. Change the variable to influence the number of parallel jobs."
 fi
 echo "variables: ${SBI_GENOME} ${SBI_NUM_THREADS}"
 
-goby ${memory_requirement} suggest-position-slices ${ALIGNMENTS} --number-of-slices 60 -o slices.tsv
+goby ${memory_requirement} suggest-position-slices ${ALIGNMENTS} --number-of-slices 60-o slices.tsv
 grep -v targetIdStart slices.tsv >slices
-echo " concatenate-alignments --genome  ${SBI_GENOME}  ${ALIGNMENTS} -x HTSJDKReaderImpl:force-sorted=true " >command.txt
 
-cut -f3,6 slices  | awk 'BEGIN{count=1} {print "-s "$1" -e " $2" -o out-part-"(count++)}' >boundaries
-parallel -j${SBI_NUM_THREADS} --plus  --progress goby ${memory_requirement}  `cat command.txt`  :::: boundaries
+nLine=0
+tail -n +2 file.txt | while read line
+    do
+       sRef=`cut -f1 line`
+       sPos=`cut -f2 line`
+       ePos=`cut -f5 line`
+       samtools view -b ${ALIGNMENTS} ${sReg}:${sPos}-${ePos} > slice_${nLine}.bam
+       echo "samtools calmd $slice_{nLine}.bam ${FASTA_GENOME} > md_slice_${nLine}.bam ;\
+        goby ${memory_requirement} concatenate-alignments --genome  ${SBI_GENOME}  md_slice_${nLine}.bam -o goby_slice_${nLine}"\
+         >> calmd-and-convert-commands.txt
+       var=$((var+1))
+       fi
+    done
+done
 
-goby ${memory_requirement} concatenate-alignments out-part-*.entries -o ${OUTPUT_BASENAME}
+for
+parallel -j${SBI_NUM_THREADS} -a --progress calmd-and-convert-commands.txt echo
+
+goby ${memory_requirement} concatenate-alignments goby_slice_*.entries -o ${OUTPUT_BASENAME}
