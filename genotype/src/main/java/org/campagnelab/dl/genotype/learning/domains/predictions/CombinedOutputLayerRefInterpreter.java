007912f158a4f4ae29d5d6b03bc4c8707b7fa1a2
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2017-01-10 17:54:50 -0500

    fixed-ref label mapper implemented. probably best to test this with the fixed ref feature mapper also.

---
+++ genotype/src/main/java/org/campagnelab/dl/genotype/learning/domains/predictions/CombinedOutputLayerRefInterpreter.java
@@ -0,0 +1,107 @@
+package org.campagnelab.dl.genotype.learning.domains.predictions;
+
+import org.campagnelab.dl.framework.domains.prediction.PredictionInterpreter;
+import org.campagnelab.dl.genotype.mappers.CombinedLabelsMapper;
+import org.campagnelab.dl.genotype.predictions.CombinedOutputLayerPrediction;
+import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
+import org.jetbrains.annotations.NotNull;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ * Interpret model output to determine if a site is homozygous/het and what hte homosygous genotype is.
+ * Created by rct66 on 11/12/16.
+ */
+public class CombinedOutputLayerRefInterpreter extends SortingCountInterpreter<CombinedOutputLayerPrediction>
+        implements PredictionInterpreter<BaseInformationRecords.BaseInformation, CombinedOutputLayerPrediction> {
+
+    private static final int MAX_GENOTYPES = CombinedLabelsMapper.NUM_LABELS;
+    public static final String NO_CALL = ".";
+    private String[] toSequences;
+    private int referenceGenotypeIndex;
+    private double probability;
+
+    public CombinedOutputLayerRefInterpreter() {
+        super(true);
+        // the default toSequences are integers, because we don't have access to exact genotypes (we have no record),
+        // but integers are sufficient to calculate performance stats.
+        toSequences = new String[MAX_GENOTYPES];
+        for (int genotypeIndex = 0; genotypeIndex < MAX_GENOTYPES; genotypeIndex++) {
+            toSequences[genotypeIndex] = Integer.toString(genotypeIndex);
+        }
+        // the other genotype can never match:
+        toSequences[3] = "-1";
+    }
+
+
+    @Override
+    public CombinedOutputLayerPrediction interpret(INDArray trueLabels, INDArray output, int predictionIndex) {
+        CombinedOutputLayerPrediction p = new CombinedOutputLayerPrediction();
+        p.trueGenotype = reconstructGenotype(trueLabels,predictionIndex);
+        p.predictedGenotype = reconstructGenotype(output,predictionIndex);
+        p.overallProbability=probability;
+        return p;
+    }
+
+    @Override
+    public CombinedOutputLayerPrediction interpret(BaseInformationRecords.BaseInformation record, INDArray output) {
+        sortedCountRecord = sort(record);
+        CombinedOutputLayerPrediction pred = new CombinedOutputLayerPrediction();
+        pred.inspectRecord(sortedCountRecord);
+        pred.predictedGenotype = getPrediction(sortedCountRecord, output);
+        pred.overallProbability = probability;
+        return pred;
+    }
+
+    public String getPrediction(BaseInformationRecords.BaseInformation record, INDArray output) {
+
+        for (int genotypeIndex = 0; genotypeIndex < MAX_GENOTYPES; genotypeIndex++) {
+            toSequences[genotypeIndex] = record.getSamples(0).getCounts(genotypeIndex).getToSequence();
+            if (record.getSamples(0).getCounts(genotypeIndex).getMatchesReference()) {
+                referenceGenotypeIndex = genotypeIndex;
+            }
+
+        }
+
+        return reconstructGenotype(output, 0);
+    }
+
+    @NotNull
+    private String reconstructGenotype(INDArray output, int predictionIndex) {
+        probability = -1;
+        int maxIndex = -1;
+        for (int i = 0; i < CombinedLabelsMapper.NUM_LABELS; i++) {
+            double outputDouble = output.getDouble(predictionIndex, i);
+            if (probability < outputDouble) {
+                maxIndex = i;
+                probability = outputDouble;
+            }
+        }
+        String homozygAllele;
+        int altIndex;
+        switch (maxIndex) {
+            case 0:
+                homozygAllele = toSequences[referenceGenotypeIndex];
+                return homozygAllele + "/" + homozygAllele;
+            case 1:
+                if (referenceGenotypeIndex == 0) {
+                    altIndex = 1;
+                } else {
+                    altIndex = 0;
+                }
+                String firstAllele = toSequences[referenceGenotypeIndex];
+                String secondAllele = toSequences[altIndex];
+                return firstAllele + "/" + secondAllele;
+            case 2:
+                if (referenceGenotypeIndex == 0) {
+                    altIndex = 1;
+                } else {
+                    altIndex = 0;
+                }
+                homozygAllele = toSequences[altIndex];
+                return homozygAllele + "/" + homozygAllele;
+            default:
+                // other site predicted, mark as wrong
+                return NO_CALL;
+        }
+    }
+}
---
+++ genotype/src/main/java/org/campagnelab/dl/genotype/mappers/CombinedLabelsMapperRef.java
@@ -0,0 +1,111 @@
+package org.campagnelab.dl.genotype.mappers;
+
+import org.campagnelab.dl.framework.mappers.MappedDimensions;
+import org.campagnelab.dl.somatic.mappers.NoMasksLabelMapper;
+import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ * A full softmax mapper that offers homozygous first, heterozygous first/second, and homozygous second no-call.
+ * Will eventually be extendible to more alternates and more alleles per genotype (ie non-diploid plants)
+ *
+ * REQUIRES sorted features
+ *
+ * Created by rct66 on 12/20/16.
+ *
+ *
+ */
+public class CombinedLabelsMapperRef extends NoMasksLabelMapper<BaseInformationRecords.BaseInformation> {
+
+    public static final int NUM_LABELS = 4;
+    private float epsilon=0;
+
+    public CombinedLabelsMapperRef(float epsilon) {
+        this.epsilon = epsilon;
+    }
+
+    public CombinedLabelsMapperRef() {
+        this(0);
+    }
+
+    @Override
+    public int numberOfLabels() {
+        return NUM_LABELS;
+    }
+
+
+    int[] indices = new int[]{0, 0};
+
+    @Override
+    public void mapLabels(BaseInformationRecords.BaseInformation record, INDArray labels, int indexOfRecord) {
+        indices[0] = indexOfRecord;
+
+        for (int labelIndex = 0; labelIndex < numberOfLabels(); labelIndex++) {
+            indices[1] = labelIndex;
+            labels.putScalar(indices, produceLabel(sortedCountRecord, labelIndex));
+        }
+    }
+
+    @Override
+    public void prepareToNormalize(BaseInformationRecords.BaseInformation record, int indexOfRecord) {
+        sortedCountRecord = sortHelper.sort(record);
+    }
+
+    private BaseInformationRecords.BaseInformation sortedCountRecord;
+    private RecordCountSortHelper sortHelper = new RecordCountSortHelper();
+
+
+    @Override
+    public float produceLabel(BaseInformationRecords.BaseInformation record, int labelIndex) {
+        int correctLabelIndex;
+        boolean firstCalled = sortedCountRecord.getSamples(0).getCounts(0).getIsCalled();
+        boolean secondCalled = sortedCountRecord.getSamples(0).getCounts(1).getIsCalled();
+        boolean firstIsRef = sortedCountRecord.getSamples(0).getCounts(0).getMatchesReference();
+        boolean secondIsRef = sortedCountRecord.getSamples(0).getCounts(1).getMatchesReference();;
+        boolean otherCalled = false;
+
+        for (int i = 2; i < sortedCountRecord.getSamples(0).getCountsCount(); i++){
+            otherCalled |= sortedCountRecord.getSamples(0).getCounts(i).getIsCalled();
+        }
+        if (otherCalled){
+            //other call case
+            correctLabelIndex = 3;
+        } else if (firstCalled && secondCalled) {
+            //both called
+            if (firstIsRef || secondIsRef){
+                correctLabelIndex = 1;
+            } else {
+                correctLabelIndex = 3;
+            }
+        } else if (firstCalled){
+            //first is called but no second
+            if (firstIsRef){
+                correctLabelIndex = 0;
+            } else {
+                correctLabelIndex = 2;
+            }
+        } else if (secondCalled){
+            //second is called but not first
+            if (secondIsRef){
+                correctLabelIndex = 0;
+            } else {
+                correctLabelIndex = 2;
+            }
+        } else {
+            //no calls at all
+            correctLabelIndex = 3;
+        }
+
+        return (labelIndex == correctLabelIndex)?1-epsilon:epsilon/(numberOfLabels()-1);
+    }
+
+
+
+
+    @Override
+    public MappedDimensions dimensions() {
+        return new MappedDimensions(numberOfLabels());
+    }
+
+
+}
