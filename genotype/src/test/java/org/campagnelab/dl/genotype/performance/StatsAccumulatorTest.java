de61fa770bab9361142eae4da7a256c66f274ff6
Joshua Cohen <jbc264@cornell.edu>
2017-02-08 15:11:01 -0500

    Fix StatsAccumulatorTest

--- genotype/src/test/java/org/campagnelab/dl/genotype/performance/StatsAccumulatorTest.java
+++ genotype/src/test/java/org/campagnelab/dl/genotype/performance/StatsAccumulatorTest.java
@@ -27,8 +27,7 @@ public class StatsAccumulatorTest {
 
 
         double[] actual = new double[]{0.5, 0.5, 0.5, 0.5, 2,0.5, Double.NaN, Double.NaN, Double.NaN, Double.NaN, 0.5,
-                0.5, 1.0, (2.0 * 0.5 * 1.0) / (0.5 + 1.0)};
-        System.out.println(Arrays.toString(stats));
+                0.5, 1.0, (2.0 * 0.5 * 1.0) / (0.5 + 1.0), 0};
         for (int i = 0; i < stats.length; i++) {
             assertEquals(actual[i], stats[i], 0.00001);
         }
