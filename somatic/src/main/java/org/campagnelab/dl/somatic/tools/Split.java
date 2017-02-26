c116eb99dd92f34608d15d81bb75bdfc41e04637
Fabien Campagne <fac2003@campagnelab.org>
2017-02-26 11:16:54 -0500

    Restore default split behavior when no destination override option is provided.

--- somatic/src/main/java/org/campagnelab/dl/somatic/tools/Split.java
+++ somatic/src/main/java/org/campagnelab/dl/somatic/tools/Split.java
@@ -7,9 +7,9 @@ import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 import org.campagnelab.dl.framework.tools.arguments.AbstractTool;
+import org.campagnelab.dl.somatic.storage.RecordReader;
 import org.campagnelab.dl.somatic.storage.RecordWriter;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
-import org.campagnelab.dl.somatic.storage.RecordReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -65,55 +65,55 @@ public class Split extends AbstractTool<SplitArguments> {
                 // exclude the chromosome from regular sampling:
                 excludedIndices.add(indexOfSuffix);
             }
+        }
+        numOutputs = args().fractions.size();
+        if (numOutputs == 1) {
+            System.err.println("Splitting a file into one fraction is not useful. Aborting.");
+            System.exit(1);
+        }
+        RecordWriter outputWriters[] = new RecordWriter[numOutputs];
 
-            numOutputs = args().fractions.size();
-            if (numOutputs == 1) {
-                System.err.println("Splitting a file into one fraction is not useful. Aborting.");
-                System.exit(1);
+        try (RecordReader reader = new RecordReader(args().inputFile)) {
+            fractions = new double[numOutputs];
+            double sumFractions = 0;
+            for (int i = 0; i < numOutputs; i++) {
+                outputWriters[i] = new RecordWriter(args().outputFile + args().suffixes.get(i));
+                fractions[i] = args().fractions.get(i);
+                sumFractions += fractions[i];
             }
-            RecordWriter outputWriters[] = new RecordWriter[numOutputs];
+            // normalize fractions:
+            for (int i = 0; i < numOutputs; i++) {
+                fractions[i] /= sumFractions;
+            }
 
-            try (RecordReader reader = new RecordReader(args().inputFile)) {
-                fractions = new double[numOutputs];
-                double sumFractions = 0;
-                for (int i = 0; i < numOutputs; i++) {
-                    outputWriters[i] = new RecordWriter(args().outputFile + args().suffixes.get(i));
-                    fractions[i] = args().fractions.get(i);
-                    sumFractions += fractions[i];
-                }
-                // normalize fractions:
-                for (int i = 0; i < numOutputs; i++) {
-                    fractions[i] /= sumFractions;
-                }
-
-                rand = new XoRoShiRo128PlusRandom();
+            rand = new XoRoShiRo128PlusRandom();
 //set up logger
-                ProgressLogger pgRead = new ProgressLogger(LOG);
-                pgRead.itemsName = "records";
-                pgRead.expectedUpdates = Math.min(reader.getTotalRecords(), args().writeN);
-                pgRead.displayFreeMemory = true;
-                pgRead.start();
-                long numWritten = 0;
-                for (BaseInformationRecords.BaseInformation record : reader) {
-                    int index = recorgBelongsTo(record);
+            ProgressLogger pgRead = new ProgressLogger(LOG);
+            pgRead.itemsName = "records";
+            pgRead.expectedUpdates = Math.min(reader.getTotalRecords(), args().writeN);
+            pgRead.displayFreeMemory = true;
+            pgRead.start();
+            long numWritten = 0;
+            for (BaseInformationRecords.BaseInformation record : reader) {
+                int index = recorgBelongsTo(record);
 
-                    outputWriters[index].writeRecord(record);
-                    pgRead.lightUpdate();
-                    numWritten += 1;
-                    if (numWritten > args().writeN) {
-                        break;
-                    }
+                outputWriters[index].writeRecord(record);
+                pgRead.lightUpdate();
+                numWritten += 1;
+                if (numWritten > args().writeN) {
+                    break;
                 }
-                pgRead.stop();
-                for (int i = 0; i < numOutputs; i++) {
-                    outputWriters[i].close();
-                }
-                reader.close();
-            } catch (IOException e) {
-                System.err.println("Unable to load or write files. Check command line arguments.");
             }
+            pgRead.stop();
+            for (int i = 0; i < numOutputs; i++) {
+                outputWriters[i].close();
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Unable to load or write files. Check command line arguments.");
         }
     }
+
 
     private int recorgBelongsTo(BaseInformationRecords.BaseInformation record) {
         final String chromosome = record.getReferenceId();
@@ -138,7 +138,7 @@ public class Split extends AbstractTool<SplitArguments> {
             }
             return index;
         } else {
-           // System.out.println("Overriding destination: " + overrideIndex);
+            // System.out.println("Overriding destination: " + overrideIndex);
             return overrideIndex;
         }
     }
