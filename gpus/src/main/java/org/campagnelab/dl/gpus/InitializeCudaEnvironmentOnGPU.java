d241ddbc390c477db0fe718495d7e145cc3cd881
Fabien Campagne <fac2003@campagnelab.org>
2017-03-09 15:46:20 -0500

    Reenable other params and cross device access.

--- gpus/src/main/java/org/campagnelab/dl/gpus/InitializeCudaEnvironmentOnGPU.java
+++ gpus/src/main/java/org/campagnelab/dl/gpus/InitializeCudaEnvironmentOnGPU.java
@@ -1,5 +1,7 @@
 package org.campagnelab.dl.gpus;
+
 import org.nd4j.jita.conf.CudaEnvironment;
+
 /**
  * Created by fac2003 on 12/1/16.
  */
@@ -10,15 +12,15 @@ public class InitializeCudaEnvironmentOnGPU {
         System.out.println("===== DEBUG IS ON =====");
         CudaEnvironment.getInstance().getConfiguration()
                 .enableDebug(true)
-                .allowMultiGPU(true);
-        //        .setMaximumGridSize(1024)
-         //       .setMaximumBlockSize(1024)
-          //      .setMaximumDeviceCacheableLength(1 * GB)
-           //     .setMaximumDeviceCache(8L * GB)
-            //    .setMaximumHostCacheableLength(1 * GB)
-             //   .setMaximumHostCache(16L * GB)
-                // cross-device access is used for faster model averaging over pcie
-            //    .allowCrossDeviceAccess(true);
+                .allowMultiGPU(true)
+                .setMaximumGridSize(1024)
+                .setMaximumBlockSize(1024)
+                .setMaximumDeviceCacheableLength(1 * GB)
+                .setMaximumDeviceCache(8L * GB)
+                .setMaximumHostCacheableLength(1 * GB)
+                .setMaximumHostCache(16L * GB)
+        // cross - device access is used for faster model averaging over pcie
+                .allowCrossDeviceAccess(true);
         System.out.println("Configured CUDA environment.");
     }
 }
