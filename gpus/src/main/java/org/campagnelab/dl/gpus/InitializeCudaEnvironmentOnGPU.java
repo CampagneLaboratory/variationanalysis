6ccf9d84e257b1590745f15ccc8664cfc5b580ae
Fabien Campagne <fac2003@campagnelab.org>
2017-03-09 14:10:44 -0500

    Revert to all defaults.

--- gpus/src/main/java/org/campagnelab/dl/gpus/InitializeCudaEnvironmentOnGPU.java
+++ gpus/src/main/java/org/campagnelab/dl/gpus/InitializeCudaEnvironmentOnGPU.java
@@ -8,16 +8,16 @@ public class InitializeCudaEnvironmentOnGPU {
 
         final long GB = 1024 * 1024 * 1024L;
         CudaEnvironment.getInstance().getConfiguration()
-                .enableDebug(false)
-                .allowMultiGPU(true)
-                .setMaximumGridSize(1024)
-                .setMaximumBlockSize(1024)
-                .setMaximumDeviceCacheableLength(1*GB)
-                .setMaximumDeviceCache(4L * GB)
-                .setMaximumHostCacheableLength(1*GB)
-                .setMaximumHostCache(16L *GB)
+                .enableDebug(true)
+                .allowMultiGPU(true);
+        //        .setMaximumGridSize(1024)
+         //       .setMaximumBlockSize(1024)
+          //      .setMaximumDeviceCacheableLength(1 * GB)
+           //     .setMaximumDeviceCache(8L * GB)
+            //    .setMaximumHostCacheableLength(1 * GB)
+             //   .setMaximumHostCache(16L * GB)
                 // cross-device access is used for faster model averaging over pcie
-                .allowCrossDeviceAccess(true);
+            //    .allowCrossDeviceAccess(true);
         System.out.println("Configured CUDA environment.");
     }
 }
