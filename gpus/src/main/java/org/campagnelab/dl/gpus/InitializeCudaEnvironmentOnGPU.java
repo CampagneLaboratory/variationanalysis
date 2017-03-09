853b7a50c7a997549b6aae104694b7ed516f9f88
Fabien Campagne <fac2003@campagnelab.org>
2017-03-09 13:42:49 -0500

    Adjust cache sizes.

--- gpus/src/main/java/org/campagnelab/dl/gpus/InitializeCudaEnvironmentOnGPU.java
+++ gpus/src/main/java/org/campagnelab/dl/gpus/InitializeCudaEnvironmentOnGPU.java
@@ -5,15 +5,18 @@ import org.nd4j.jita.conf.CudaEnvironment;
  */
 public class InitializeCudaEnvironmentOnGPU {
     public void InitializeCudaEnvironmentOnGPU() {
+        // temp workaround for backend initialization
+        Nd4j.create(1);
+        final long GB = 1024 * 1024 * 1024L;
         CudaEnvironment.getInstance().getConfiguration()
                 .enableDebug(false)
                 .allowMultiGPU(true)
                 .setMaximumGridSize(1024)
                 .setMaximumBlockSize(1024)
-                .setMaximumDeviceCacheableLength(1024 * 1024 * 1024L)
-                .setMaximumDeviceCache(8L * 1024 * 1024 * 1024L)
-                .setMaximumHostCacheableLength(1024 * 1024 * 1024L)
-                .setMaximumHostCache(8L * 1024 * 1024 * 1024L)
+                .setMaximumDeviceCacheableLength(1*GB)
+                .setMaximumDeviceCache(4L * GB)
+                .setMaximumHostCacheableLength(1*GB)
+                .setMaximumHostCache(16L *GB)
                 // cross-device access is used for faster model averaging over pcie
                 .allowCrossDeviceAccess(true);
         System.out.println("Configured CUDA environment.");
