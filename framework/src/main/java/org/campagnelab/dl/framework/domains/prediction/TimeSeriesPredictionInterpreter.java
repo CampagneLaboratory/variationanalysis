344486dba150321d5c7f7ec4219e9ac518f8fa68
Joshua Cohen <jbc264@cornell.edu>
2017-03-17 18:58:28 -0400

    Trim predictions to length of true labels during training and predict phases. Raises stats during training and fixes scoring during predict, but doesn't account for score disparity.

--- framework/src/main/java/org/campagnelab/dl/framework/domains/prediction/TimeSeriesPredictionInterpreter.java
+++ framework/src/main/java/org/campagnelab/dl/framework/domains/prediction/TimeSeriesPredictionInterpreter.java
@@ -1,6 +1,7 @@
 package org.campagnelab.dl.framework.domains.prediction;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 
 import java.util.function.Function;
 
@@ -19,7 +20,9 @@ public class TimeSeriesPredictionInterpreter<RecordType> implements PredictionIn
     public TimeSeriesPrediction interpret(RecordType record, INDArray output) {
         int[] trueLabels = recordToLabel.apply(record);
         TimeSeriesPrediction prediction = new TimeSeriesPrediction();
-        prediction.setPredictedLabels(output);
+        // TODO: Make sure trimming the output doesn't break anything
+        INDArray trimmedOutput = output.get(NDArrayIndex.all(), NDArrayIndex.interval(0, trueLabels.length));
+        prediction.setPredictedLabels(trimmedOutput);
         prediction.setTrueLabels(trueLabels);
         return prediction;
     }
@@ -30,5 +33,20 @@ public class TimeSeriesPredictionInterpreter<RecordType> implements PredictionIn
         prediction.setPredictedLabels(output, predictionIndex);
         prediction.setTrueLabels(trueLabels, predictionIndex);
         return prediction;
+    }
+
+    public TimeSeriesPrediction interpret(INDArray trueMasks, INDArray trueLabels, INDArray output, int predictionIndex) {
+        assert trueLabels.shape().length == 3 : "True labels should be a 3D array";
+        assert trueMasks.shape().length == 3 : "True masks should be a 3D array";
+        assert output.shape().length == 3 : "True labels should be a 3D array";
+        assert predictionIndex < trueMasks.shape()[0] : "prediction index is out of bounds for true masks";
+        assert predictionIndex < trueLabels.shape()[0] : "prediction index is out of bounds for true labels";
+        assert predictionIndex < output.shape()[0] : "prediction index is out of bounds for output";
+        int maxTrueLabels = trueMasks.getRow(predictionIndex).gt(0).sumNumber().intValue();
+        INDArray trimmedTrueLabels = trueLabels.get(NDArrayIndex.all(), NDArrayIndex.all(),
+                NDArrayIndex.interval(0, maxTrueLabels));
+        INDArray trimmedOutput = output.get(NDArrayIndex.all(), NDArrayIndex.all(),
+                NDArrayIndex.interval(0, maxTrueLabels));
+        return interpret(trimmedTrueLabels, trimmedOutput, predictionIndex);
     }
 }
