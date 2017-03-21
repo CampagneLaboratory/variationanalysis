bca289365a90dcfc0a897e0d2ba62534cc68f837
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2017-03-21 15:34:11 -0400

    update parallel gatk script with fixes.

--- bin/parallel-gatk-realigner.sh
+++ bin/parallel-gatk-realigner.sh
@@ -1,9 +1,9 @@
-GATK_JAR=$0
-MEMORY_PER_THREAD=$1
-NUM_THREADS=$2
-GENOME_FA=$3
-BAM_INPUT=$4
-BAM_OUTPUT=$5
+GATK_JAR=$1
+MEMORY_PER_THREAD=$2
+NUM_THREADS=$3
+GENOME_FA=$4
+BAM_INPUT=$5
+BAM_OUTPUT=$6
 
 DATE=`date +%Y-%m-%d`
 echo ${DATE} >DATE.txt
@@ -16,15 +16,11 @@ echo "Using ${GENOME_FA} fasta file as genome. (index file .fa.fai required)."
 echo "Using ${BAM_INPUT} as bam input."
 echo "Writing bam output to ${BAM_OUTPUT}."
 
-echo "variables: ${SBI_GENOME} ${SBI_NUM_THREADS}"
-
-
-
-if [! -f ${BAM_INPUT}.bai]; then
+if [ ! -f ${BAM_INPUT}.bai ]; then
     samtools index ${BAM_INPUT}
-if
+fi
 
-goby 8g suggest-position-slices ${ALIGNMENTS} --number-of-slices 60 -o slices.tsv --restrict-per-chromosome
+goby ${MEMORY_PER_THREAD} suggest-position-slices ${BAM_INPUT} --number-of-slices 60 -o slices.tsv --restrict-per-chromosome
 
 rm -rf calmd-and-convert-commands.txt
 nLine=0
@@ -33,8 +29,8 @@ tail -n +2 slices.tsv | while read -r line
        sRef=`echo $line | cut -f1 -d ' '`
        sPos=`echo $line | cut -f2 -d ' '`
        ePos=`echo $line | cut -f5 -d ' '`
-       echo "samtools view -u ${ALIGNMENTS} ${sRef}:${sPos}-${ePos} > slice_${nLine}.bam ;\
-         samtools calmd -E -u slice_${nLine}.bam ${FASTA_GENOME} > md_slice_${nLine}.bam ;\
+       echo "samtools view -u ${BAM_INPUT} ${sRef}:${sPos}-${ePos} > slice_${nLine}.bam ;\
+         samtools calmd -E -u slice_${nLine}.bam ${GENOME_FA} > md_slice_${nLine}.bam ;\
          samtools index md_slice_${nLine}.bam &&\
          rm slice_${nLine}.bam ;\
          java -Xmx${MEMORY_PER_THREAD} -jar ${GATK_JAR} -T HaplotypeCaller -R ${GENOME_FA} -I md_slice_${nLine}.bam -o hc_variants.vcf  -bamout realigned_md_slice_${nLine}.bam &&\
