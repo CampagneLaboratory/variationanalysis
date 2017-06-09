8d01987f89acb0c97985bc3b0f90f0fe8a299184
Fabien Campagne <fac2003@campagnelab.org>
2017-06-09 09:13:53 -0400

    Add covariates file.

--- bin/parallel-somatic-calls.sh
+++ bin/parallel-somatic-calls.sh
@@ -7,13 +7,15 @@ assertBctftoolsInstalled
 . ./configure.sh
 
 # Call somatic variations across one or more alignments, using DL variationanalysis models and Goby3:
-# Usage: parallel-somatic-calls.sh 10g somatic_model genotype_model alignment.entries [alignment.entries]+
-SOMATIC_MODEL_PATH=$1
-GENOTYPE_MODEL_PATH=$2
+# Usage: parallel-somatic-calls.sh 10g covariates.tsv somatic_model genotype_model alignment.entries [alignment.entries]+
+GOBY_DSV_COVARIATES=$1
+SOMATIC_MODEL_PATH=$2
+GENOTYPE_MODEL_PATH=$3
+shift
 shift
 shift
 ALIGNMENTS="$*"
 
-export GOBY_DSV_OPTIONS=" -x SomaticVariationOutputFormat:model-path=${SOMATIC_MODEL_PATH} "
+export GOBY_DSV_OPTIONS=" --covariates ${GOBY_DSV_COVARIATES} -x SomaticVariationOutputFormat:model-path=${SOMATIC_MODEL_PATH} "
 export GOBY_GENOTYPE_FORMAT="SOMATIC_VARIATIONS"
 parallel-calls.sh ${memory_requirement} ${GENOTYPE_MODEL_PATH}
