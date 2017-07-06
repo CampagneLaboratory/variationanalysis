94c18246dd81fc92bb024fe4236ad24d21b2a9ed
Fabien Campagne <fac2003@campagnelab.org>
2017-07-06 08:42:20 -0400

    Add focus varmap option to parallel somatic script.

--- bin/parallel-somatic-calls.sh
+++ bin/parallel-somatic-calls.sh
@@ -15,6 +15,15 @@ shift
 shift
 shift
 ALIGNMENTS="$*"
-export GOBY_DSV_OTHER_OPTIONS=" --covariates ${GOBY_DSV_COVARIATES} -x SomaticVariationOutputFormat:model-path=${SOMATIC_MODEL_PATH} "
+
+
+if [ -z "${FOCUS_VARMAP+set}" ]; then
+    FOCUS_VARMAP_OPTION=" "
+    echo "FOCUS_VARMAP not set.  Set the variable to path of varmap to restrict output to only sites in the varmap."
+else
+    FOCUS_VARMAP_OPTION=" -x SomaticVariationOutputFormat:focus-on-varmap=${FOCUS_VARMAP} "
+fi
+
+export GOBY_DSV_OTHER_OPTIONS=" --covariates ${GOBY_DSV_COVARIATES} ${FOCUS_VARMAP_OPTION} -x SomaticVariationOutputFormat:model-path=${SOMATIC_MODEL_PATH} "
 export GOBY_GENOTYPE_FORMAT="SOMATIC_VARIATIONS"
 parallel-calls.sh ${memory_requirement} ${GENOTYPE_MODEL_PATH} ${ALIGNMENTS}
