cd0659209a3aa1a169c10b616d4e81633792938d
Fabien Campagne <fac2003@campagnelab.org>
2017-07-06 13:42:55 -0400

    Define somatic threshold as a variable.

--- bin/parallel-somatic-calls.sh
+++ bin/parallel-somatic-calls.sh
@@ -21,7 +21,6 @@ shift
 shift
 ALIGNMENTS="$*"
 
-
 if [ -z "${FOCUS_VARMAP+set}" ]; then
     FOCUS_VARMAP_OPTION=" "
     echo "FOCUS_VARMAP not set.  Set the variable to path of varmap to restrict output to only sites in the varmap."
@@ -29,6 +28,12 @@ else
     FOCUS_VARMAP_OPTION=" -x SomaticVariationOutputFormat:focus-on-varmap=${FOCUS_VARMAP} "
 fi
 
-export GOBY_DSV_OTHER_OPTIONS=" --covariates ${GOBY_DSV_COVARIATES} ${FOCUS_VARMAP_OPTION} -x SomaticVariationOutputFormat:model-path=${SOMATIC_MODEL_PATH} "
+
+if [ -z "${SOMATIC_P_THRESHOLD+set}" ]; then
+    SOMATIC_P_THRESHOLD="0.9"
+    echo "Set variable SOMATIC_P_THRESHOLD to a P value threshold to adjust threshold. Currently using threshold=${SOMATIC_P_THRESHOLD}"
+fi
+
+export GOBY_DSV_OTHER_OPTIONS=" --covariates ${GOBY_DSV_COVARIATES} -x SomaticVariationOutputFormat:model-p-mutated-threshold=${SOMATIC_P_THRESHOLD} -x SomaticVariationOutputFormat:model-path=${SOMATIC_MODEL_PATH} "
 export GOBY_GENOTYPE_FORMAT="SOMATIC_VARIATIONS"
 parallel-calls.sh ${memory_requirement} ${GENOTYPE_MODEL_PATH} ${ALIGNMENTS}
