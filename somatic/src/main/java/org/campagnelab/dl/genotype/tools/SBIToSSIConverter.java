fff2c75d6603a0386e2ee35267fe54474dbbadd7
Manuele Simi <manuele.simi@campagnelab.org>
2017-10-12 15:29:59 -0400

    Refactor the segment structure to hold in memory the data for each segment. Move this part outside the SBIToSSI tool.

--- somatic/src/main/java/org/campagnelab/dl/genotype/tools/SBIToSSIConverter.java
+++ somatic/src/main/java/org/campagnelab/dl/genotype/tools/SBIToSSIConverter.java
@@ -1,5 +1,6 @@
 package org.campagnelab.dl.genotype.tools;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.campagnelab.dl.framework.tools.arguments.AbstractTool;
 import org.campagnelab.dl.somatic.storage.RecordReader;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
@@ -10,6 +11,10 @@ import org.campagnelab.goby.util.FileExtensionHelper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Tool to convert from SBI to SSI format.
@@ -20,7 +25,7 @@ public class SBIToSSIConverter extends AbstractTool<SBIToSSIConverterArguments> 
 
     SequenceSegmentInformationWriter writer = null;
 
-    final SegmentHolder currentSegment = new SegmentHolder();
+    SegmentList segmentList ;
 
     public static void main(String[] args) {
         SBIToSSIConverter tool = new SBIToSSIConverter();
@@ -61,15 +66,18 @@ public class SBIToSSIConverter extends AbstractTool<SBIToSSIConverterArguments> 
 
     private void manageRecord(BaseInformationRecords.BaseInformation record, int gap) {
         int position = record.getPosition();
-        if (position - currentSegment.getCurrentLastLocation() > gap) {
-            //
-           currentSegment.newSegment(record);
+        if (segmentList == null) {
+            segmentList = new SegmentList(record);
+        } else {
+            if (position - segmentList.getCurrentLocation() > gap) {
+                segmentList.newSegment(record);
+            }
+            segmentList.add(record);
         }
-        currentSegment.add(record);
     }
 
     private void closeOutput() {
-       currentSegment.close();
+        segmentList.close();
         try {
             writer.close();
         } catch (IOException e) {
@@ -80,91 +88,6 @@ public class SBIToSSIConverter extends AbstractTool<SBIToSSIConverterArguments> 
         }
     }
 
-    class SegmentHolder {
-        private SegmentInformationRecords.SegmentInformation.Builder builder =
-                SegmentInformationRecords.SegmentInformation.newBuilder();
-        private int currentLastPosition = 0;
-        private int currentLastReferenceIndex = 0;
-        private String currentLastReferenceId = "";
-
-        /**
-         * Creates a new segment and eventually closes the previous one.
-         * @param first the first record of the new segment 
-         */
-        protected void newSegment(BaseInformationRecords.BaseInformation first) {
-            this.close();
-            System.out.println("Open a new segment at position " + Integer.toString(first.getPosition()));
-            builder = SegmentInformationRecords.SegmentInformation.newBuilder();
-            SegmentInformationRecords.ReferencePosition.Builder refBuilder = SegmentInformationRecords.ReferencePosition.newBuilder();
-            refBuilder.setLocation(first.getPosition());
-            refBuilder.setReferenceIndex(first.getReferenceIndex());
-            refBuilder.setReferenceId(first.getReferenceId());
-            builder.setStartPosition(refBuilder);
-            setAsLast(first);
-        }
-
-        protected void close() {
-            if (builder != null) {
-                //close the previous segment
-                System.out.println("Close the segment.");
-                try {
-                    writer.appendEntry(builder.build());
-                    //set the current* as end position
-                    SegmentInformationRecords.ReferencePosition.Builder refBuilder = SegmentInformationRecords.ReferencePosition.newBuilder();
-                    refBuilder.setLocation(currentLastPosition);
-                    refBuilder.setReferenceIndex(currentLastReferenceIndex);
-                    refBuilder.setReferenceId(currentLastReferenceId);
-                    builder.setEndPosition(refBuilder);
-                } catch (IOException e) {
-                    System.err.println("Unable to close the previous segment");
-                    e.printStackTrace();
-                } finally {
-                    builder = null;
-                    currentLastPosition = 0;
-                    currentLastReferenceId = "";
-                    currentLastReferenceIndex = 0;
-                }
-                //create statistics here.
-            }
-        }
-
-        public int getCurrentLastLocation() {
-            return currentLastPosition;
-        }
-
-        /**
-         * Adds a record to the current segment
-         * @param record
-         */
-        public void add(BaseInformationRecords.BaseInformation record) {
-            record.getSamplesList().forEach(sampleInfo -> {
-                    SegmentInformationRecords.Sample.Builder sampleBuilder = SegmentInformationRecords.Sample.newBuilder();
-                    SegmentInformationRecords.Base.Builder baseBuilder = SegmentInformationRecords.Base.newBuilder();
-                    //TODO: set real values here
-                    baseBuilder.addFeatures(1f);
-                    baseBuilder.addLabels(2f);
-                    baseBuilder.addTrueLabel("foo");
-                    sampleBuilder.addBase(baseBuilder);
-                    builder.addSample(sampleBuilder);
-                }
-            );
-            SegmentInformationRecords.ReferencePosition.Builder refBuilder = SegmentInformationRecords.ReferencePosition.newBuilder();
-            refBuilder.setLocation(record.getPosition());
-            refBuilder.setReferenceIndex(record.getReferenceIndex());
-            refBuilder.setReferenceId(record.getReferenceId());
-            setAsLast(record);
-        }
-
-        /**
-         * Sets the record as the last one in the current segment.
-         * @param record
-         */
-        private void setAsLast(BaseInformationRecords.BaseInformation record) {
-            currentLastPosition = record.getPosition();
-            currentLastReferenceIndex = record.getReferenceIndex();
-            currentLastReferenceId = record.getReferenceId();
-        }
-    }
 
 
 }
---
+++ somatic/src/main/java/org/campagnelab/dl/genotype/tools/SegmentList.java
@@ -0,0 +1,150 @@
+package org.campagnelab.dl.genotype.tools;
+
+import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
+
+import java.util.Iterator;
+
+/**
+ * Segment list for SSI.
+ *
+ * @author manuele
+ */
+public class SegmentList implements Iterable<SegmentList.Segment>{
+
+    private Segment currentSegment;
+
+    private int currentLastPosition = 0;
+    private int currentLastReferenceIndex = 0;
+    private String currentLastReferenceId = "";
+
+
+    protected SegmentList(BaseInformationRecords.BaseInformation from) {
+        this.newSegment(from);
+    }
+
+
+    public int getCurrentLocation() {
+        return currentLastPosition;
+    }
+
+    /**
+     * Opens a new segment from the record.
+     * @param from
+     */
+    public void newSegment(BaseInformationRecords.BaseInformation from) {
+        if (currentSegment != null)
+            this.closeSegment();
+        currentSegment = new Segment(from);
+        this.add(from);
+
+    }
+
+    public void closeSegment() {
+       currentSegment.close();
+       currentSegment.printStats();
+    }
+
+    public void add(BaseInformationRecords.BaseInformation record) {
+       currentSegment.add(record);
+       setAsLast(record);
+    }
+
+    /**
+     * Sets the record as the last one in the current segment.
+     * @param record
+     */
+    private void setAsLast(BaseInformationRecords.BaseInformation record) {
+        currentLastPosition = record.getPosition();
+        currentLastReferenceIndex = record.getReferenceIndex();
+        currentLastReferenceId = record.getReferenceId();
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<Segment> iterator() {
+        return null;
+    }
+
+    public void close() {
+        this.closeSegment();
+        this.printStats();
+    }
+
+    private void printStats() {
+        
+    }
+
+    /**
+     * Holds the current open segment before it is stored in the list.
+     */
+    class Segment {
+        private int startPosition = 0;
+        private int endPosition = 0;
+
+        Segment(BaseInformationRecords.BaseInformation first) {
+            System.out.println("Open a new segment at position " + Integer.toString(first.getPosition()));
+            this.startPosition = first.getPosition();
+            this.endPosition = first.getPosition();
+        }
+
+        protected void close() {
+            //if (builder != null) {
+                //close the previous segment
+                System.out.println("Close the segment.");
+                 /*try {
+                   writer.appendEntry(builder.build());
+                    //set the current* as end position
+                    SegmentInformationRecords.ReferencePosition.Builder refBuilder = SegmentInformationRecords.ReferencePosition.newBuilder();
+                    refBuilder.setLocation(currentLastPosition);
+                    refBuilder.setReferenceIndex(currentLastReferenceIndex);
+                    refBuilder.setReferenceId(currentLastReferenceId);
+                    builder.setEndPosition(refBuilder);
+                    printSegmentStats();
+                } catch (IOException e) {
+                    System.err.println("Unable to close the previous segment");
+                    e.printStackTrace();
+                } finally {
+                    builder = null;
+                    currentLastPosition = 0;
+                    currentLastReferenceId = "";
+                    currentLastReferenceIndex = 0;
+                }   */
+                //create statistics here.
+            //}
+        }
+
+        protected void printStats() {
+            System.out.println("Start position:" + startPosition);
+            System.out.println("End position:" + endPosition);
+            System.out.println("Length:" + (startPosition - endPosition + 1));
+        }
+
+        /**
+         * Adds a record to the current segment
+         * @param record
+         */
+        protected void add(BaseInformationRecords.BaseInformation record) {
+            record.getSamplesList().forEach(sampleInfo -> {
+                        /*SegmentInformationRecords.Sample.Builder sampleBuilder = SegmentInformationRecords.Sample.newBuilder();
+                        SegmentInformationRecords.Base.Builder baseBuilder = SegmentInformationRecords.Base.newBuilder();
+                        //TODO: set real values here
+                        baseBuilder.addFeatures(1f);
+                        baseBuilder.addLabels(2f);
+                        baseBuilder.addTrueLabel("foo");
+                        sampleBuilder.addBase(baseBuilder);
+                        builder.addSample(sampleBuilder);*/
+                    }
+            );
+            /*SegmentInformationRecords.ReferencePosition.Builder refBuilder = SegmentInformationRecords.ReferencePosition.newBuilder();
+            refBuilder.setLocation(record.getPosition());
+            refBuilder.setReferenceIndex(record.getReferenceIndex());
+            refBuilder.setReferenceId(record.getReferenceId()); */
+            this.endPosition = record.getPosition();
+
+        }
+    }
+}
