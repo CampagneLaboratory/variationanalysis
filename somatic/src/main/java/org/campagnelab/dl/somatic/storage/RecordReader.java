46917bf6d9352b90ece5cb961537db416e6680e9
Manuele Simi <manuele.simi@campagnelab.org>
2017-10-18 11:41:21 -0400

    Remove empty overridden method forEach from the record reader.

--- somatic/src/main/java/org/campagnelab/dl/somatic/storage/RecordReader.java
+++ somatic/src/main/java/org/campagnelab/dl/somatic/storage/RecordReader.java
@@ -85,28 +85,6 @@ public class RecordReader implements Closeable, RecordIterable<BaseInformationRe
     }
 
     /**
-     * Performs the given action for each element of the {@code Iterable}
-     * until all elements have been processed or the action throws an
-     * exception.  Unless otherwise specified by the implementing class,
-     * actions are performed in the order of iteration (if an iteration order
-     * is specified).  Exceptions thrown by the action are relayed to the
-     * caller.
-     *
-     * @param action The action to be performed for each element
-     * @throws NullPointerException if the specified action is null
-     * @implSpec <p>The default implementation behaves as if:
-     * <pre>{@code
-     *     for (T t : this)
-     *         action.accept(t);
-     * }</pre>
-     * @since 1.8
-     */
-    @Override
-    public void forEach(Consumer<? super BaseInformationRecords.BaseInformation> action) {
-
-    }
-
-    /**
      * Creates a {@link Spliterator} over the elements described by this
      * {@code Iterable}.
      *
