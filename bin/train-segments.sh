08c6e22fa3f15ad12f158bdc39cdc84d1978408b
Fabien Campagne <fac2003@campagnelab.org>
2017-10-18 18:22:10 -0400

    Fix classpath.

--- bin/train-segments.sh
+++ bin/train-segments.sh
@@ -2,4 +2,4 @@
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
 java -Djava.io.tmpdir=${TMPDIR} -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG}   \
-    org.campagnelab.dl.genotype.learning.TrainModelGS ${other_parameters}
+    org.campagnelab.dl.genotype.tools.TrainModelGS ${other_parameters}
