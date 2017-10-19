5e0383f2105ae5a2271ef67a489e7757c01977b5
Fabien Campagne <fac2003@campagnelab.org>
2017-10-19 14:57:25 -0400

    Define properties to configure parallel wrapper.

--- bin/train-segments.sh
+++ bin/train-segments.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
-java -Djava.io.tmpdir=${TMPDIR} -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG}   \
+java -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
+     -Dframework.parallelWrapper.prefetchBuffer=8 -Dframework.parallelWrapper.averagingFrequency=7 -Dframework.parallelWrapper.numWorkers=8  \
+      -Djava.io.tmpdir=${TMPDIR} \
     org.campagnelab.dl.genotype.tools.TrainModelGS ${other_parameters}
