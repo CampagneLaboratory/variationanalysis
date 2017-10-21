fd0aef497a2deeb339bd2f2bca99f3a435a7b93d
Fabien Campagne <fac2003@campagnelab.org>
2017-10-21 12:49:15 -0400

    Reduce prefetch.

--- bin/train-segments.sh
+++ bin/train-segments.sh
@@ -2,6 +2,6 @@
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
 java -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
-     -Dframework.parallelWrapper.prefetchBuffer=32 -Dframework.parallelWrapper.averagingFrequency=7 -Dframework.parallelWrapper.numWorkers=8   \
+     -Dframework.parallelWrapper.prefetchBuffer=16 -Dframework.parallelWrapper.averagingFrequency=7 -Dframework.parallelWrapper.numWorkers=8   \
       -Djava.io.tmpdir=${TMPDIR} \
     org.campagnelab.dl.genotype.tools.TrainModelGS ${other_parameters}
