b539e3174df903e83099eb6f1c5a7c9545435382
Fabien Campagne <fac2003@campagnelab.org>
2017-10-28 12:14:37 -0400

    Enable assertions when converting sbi to ssi.

--- bin/sbi-to-ssi.sh
+++ bin/sbi-to-ssi.sh
@@ -2,5 +2,5 @@
 FORCE_PLATFORM=native
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
-java -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
+java -ea -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
     org.campagnelab.dl.genotype.tools.SBIToSSIConverter ${other_parameters}
