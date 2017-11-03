602adc912f0816d421928413985b52fcb9dce7af
Fabien Campagne <fac2003@campagnelab.org>
2017-11-03 11:36:00 -0400

    Use createUninitializedDetached also in adapter, reading from cache.

--- framework/src/main/java/org/campagnelab/dl/framework/iterators/MultiDataSetIteratorAdapter.java
+++ framework/src/main/java/org/campagnelab/dl/framework/iterators/MultiDataSetIteratorAdapter.java
@@ -84,23 +84,23 @@ public abstract class MultiDataSetIteratorAdapter<RecordType> implements MultiDa
                 }
                 inputShape[1]++;
             }
-            inputs[index] = Nd4j.create(inputShape, 'f');
+            inputs[index] = Nd4j.createUninitializedDetached(inputShape, 'f');
             inputs[index].detach();
             featureMappers[index] = domainDescriptor.getFeatureMapper(input);
             boolean needMask = featureMappers[index].hasMask();
-            inputMasks[index] = needMask ? Nd4j.create(domainDescriptor.getInputMaskShape(size, input), 'f') : null;
+            inputMasks[index] = needMask ? Nd4j.createUninitializedDetached(domainDescriptor.getInputMaskShape(size, input), 'f') : null;
             inputMasks[index].detach();
             index += 1;
             hasFeatureMask |= needMask;
         }
         index = 0;
         for (String label : domainDescriptor.getComputationalGraph().getOutputNames()) {
-            labels[index] = Nd4j.create(domainDescriptor.getLabelShape(size, label), 'f');
+            labels[index] = Nd4j.createUninitializedDetached(domainDescriptor.getLabelShape(size, label), 'f');
             labels[index].detach();
             labelMappers[index] = domainDescriptor.getLabelMapper(label);
             boolean needMask = labelMappers[index].hasMask();
             if (needMask) {
-                labelMasks[index] = Nd4j.create(domainDescriptor.getLabelMaskShape(size, label), 'f');
+                labelMasks[index] = Nd4j.createUninitializedDetached(domainDescriptor.getLabelMaskShape(size, label), 'f');
                 labelMasks[index].detach();
             }
             index++;
