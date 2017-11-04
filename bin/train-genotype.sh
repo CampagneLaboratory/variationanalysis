1db270711bdfb26c9cb6714644edff83c6e31b83
Fabien Campagne <fac2003@campagnelab.org>
2017-11-04 12:14:02 -0400

    Define properties for training genotypes on gpu box.

--- bin/train-genotype.sh
+++ bin/train-genotype.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
-java -Djava.io.tmpdir=${TMPDIR} -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG}   \
+java -Djava.io.tmpdir=${TMPDIR} -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
+    -Dframework.parallelWrapper.prefetchBuffer=16 \
+     -Dframework.parallelWrapper.averagingFrequency=7 \
+     -Dframework.parallelWrapper.numWorkers=3   \
     org.campagnelab.dl.genotype.learning.TrainModelG ${other_parameters}
