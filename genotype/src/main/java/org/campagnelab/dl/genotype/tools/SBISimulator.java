fa9fa4c9cfcc0d0ef05de41169888161fdf60033
Manuele Simi <manuele.simi@campagnelab.org>
2017-11-06 16:20:42 -0500

    Adjust genotype creation. Generate counts from a random integer. Need to be validated.

--- genotype/src/main/java/org/campagnelab/dl/genotype/tools/SBISimulator.java
+++ genotype/src/main/java/org/campagnelab/dl/genotype/tools/SBISimulator.java
@@ -12,14 +12,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
- * Simulator for SBI files generated starting by a variant map.
+ * Generate SBI files starting from a variant map.
  */
 public class SBISimulator extends AbstractTool<SBISimulatorArguments> {
 
     static private Logger LOG = LoggerFactory.getLogger(SBISimulator.class);
+    private Random r = new Random();
 
     public static void main(String[] args) {
         SBISimulator tool = new SBISimulator();
@@ -34,23 +37,31 @@ public class SBISimulator extends AbstractTool<SBISimulatorArguments> {
 
     @Override
     public void execute() {
-        try(RecordWriter writer = new RecordWriter(args().outputFilename)) {
+        String countFormat = "%s/%s=%d+%d";
+        try (RecordWriter writer = new RecordWriter(args().outputFilename)) {
             final VariantMapHelper helper = new VariantMapHelper(args().inputFile);
             List<String> chromosomes = this.chromosomesForSBI(helper);
-            final RecordWriter finalWriter = writer;
             chromosomes.parallelStream().limit(args().readN).forEach((String chromosome) -> {
                 System.out.println("Chrom: " + chromosome);
-                for (ObjectIterator<Variant> it = helper.getAllVariants(chromosome); it.hasNext();)  {
+                for (ObjectIterator<Variant> it = helper.getAllVariants(chromosome); it.hasNext(); ) {
                     Variant variant = it.next();
-                    BaseInformationRecords.BaseInformation record = makeRecord(variant.referenceIndex, variant.position, "A/A", "A/A=12+10");
-                    try {
-                        finalWriter.writeRecord(record);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    for (Variant.FromTo trueAllele : variant.trueAlleles) {
+                        String[] counts;
+                        String genotype = String.format("%s/%s", trueAllele.getFrom(), trueAllele.getTo());
+                        if (trueAllele.getFrom().equals(trueAllele.getTo())) {
+                            counts = new String[]{
+                                    String.format(countFormat, trueAllele.getFrom(), trueAllele.getTo(), generateCounts(), generateCounts())};
+                        } else {
+                            counts = new String[]{
+                                    String.format(countFormat, trueAllele.getFrom(), trueAllele.getFrom(), generateCounts(), generateCounts()),
+                                    String.format(countFormat, trueAllele.getFrom(), trueAllele.getTo(), generateCounts(), generateCounts())};
+                        }
+                        System.out.println("Genotype=" + genotype + ", counts: " + Arrays.toString(counts));
+                        BaseInformationRecords.BaseInformation record = makeRecord(variant.referenceIndex, variant.position, genotype, counts);
+                        addRecord(writer, record);
                     }
                 }
             });
-
             writer.close();
         } catch (IOException e) {
             LOG.error("Unable to locate the variant map.");
@@ -58,16 +69,17 @@ public class SBISimulator extends AbstractTool<SBISimulatorArguments> {
         } catch (ClassNotFoundException e) {
             LOG.error("Unable to load the variant map.");
             throw new IllegalStateException("Unable to load the variant map.");
-        } 
+        }
     }
 
     /**
      * Detect the chromosomes to use for the output SBI
+     *
      * @param helper
      * @return
      */
-    private ObjectArrayList<String> chromosomesForSBI(final VariantMapHelper helper) {
-        ObjectArrayList<String> chroms = null;
+    private List<String> chromosomesForSBI(final VariantMapHelper helper) {
+        List<String> chroms = null;
         if (args().chromosome != null) {
             //need to override what is in the map
             chroms = new ObjectArrayList<String>(1);
@@ -109,6 +121,18 @@ public class SBISimulator extends AbstractTool<SBISimulatorArguments> {
         sample.setFormattedCounts(FormatterCountHelper.format(sample));
         builder.addSamples(sample);
         return builder.build();
+    }
+
+    private void addRecord(RecordWriter writer, BaseInformationRecords.BaseInformation record) {
+        try {
+            writer.writeRecord(record);
+        } catch (IOException e) {
+            LOG.error("Unable to write the record.", e);
+        }
+    }
+
+    private int generateCounts() {
+        return r.nextInt(100);
     }
 
 }
