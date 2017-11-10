33fef135aa5aff7740fa0468a77845204df19ed0
Fabien Campagne <fac2003@campagnelab.org>
2017-11-10 18:16:08 -0500

    Add workspace for validation.

--- genotype/src/main/java/org/campagnelab/dl/genotype/performance/GenotypeTrainingPerformanceHelper.java
+++ genotype/src/main/java/org/campagnelab/dl/genotype/performance/GenotypeTrainingPerformanceHelper.java
@@ -7,9 +7,14 @@ import org.campagnelab.dl.genotype.helpers.GenotypeHelper;
 import org.campagnelab.dl.genotype.predictions.GenotypePrediction;
 import org.campagnelab.dl.varanalysis.protobuf.BaseInformationRecords;
 import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.nd4j.linalg.api.memory.MemoryWorkspace;
+import org.nd4j.linalg.api.memory.conf.WorkspaceConfiguration;
+import org.nd4j.linalg.api.memory.enums.AllocationPolicy;
+import org.nd4j.linalg.api.memory.enums.LearningPolicy;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.MultiDataSet;
 import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +26,16 @@ import java.util.function.Predicate;
  */
 public class GenotypeTrainingPerformanceHelper extends PredictWithModel<BaseInformationRecords.BaseInformation> {
 
+    private final WorkspaceConfiguration learningConfig;
     protected StatsAccumulator accumulator;
 
 
     public GenotypeTrainingPerformanceHelper(DomainDescriptor<BaseInformationRecords.BaseInformation> domainDescriptor) {
         super(domainDescriptor);
+        learningConfig = WorkspaceConfiguration.builder()
+                .policyAllocation(AllocationPolicy.STRICT) // <-- this option disables overallocation behavior
+                .policyLearning(LearningPolicy.FIRST_LOOP) // <-- this option makes workspace learning after first loop
+                .build();
     }
 
     public double estimateWithGraph(MultiDataSetIterator iterator,
@@ -36,10 +46,12 @@ public class GenotypeTrainingPerformanceHelper extends PredictWithModel<BaseInfo
         });
     }
 
+
     public double estimateWithGraph(MultiDataSetIterator iterator,
                                     ComputationGraph graph,
                                     Predicate<Integer> stopIfTrue,
                                     Consumer<GenotypePrediction> observer, Consumer<Double> scoreObserver) {
+
         iterator.reset();
         accumulator = new StatsAccumulator();
         accumulator.initializeStats();
@@ -49,43 +61,47 @@ public class GenotypeTrainingPerformanceHelper extends PredictWithModel<BaseInfo
 
         List<Prediction> predictions = new ArrayList<>();
         while (iterator.hasNext()) {
-            MultiDataSet next = iterator.next();
-            INDArray[] outputs = graph.output(next.getFeatures());
-            double dsScore = graph.score(next);
-            if (dsScore == dsScore) {
-                scoreObserver.accept(dsScore);
-            }
-            INDArray[] trueLabels = next.getLabels();
+            try (MemoryWorkspace ws = Nd4j.getWorkspaceManager().getAndActivateWorkspace(learningConfig, "VALIDATION")) {
+                ws.notifyScopeEntered();
 
-            int numExamples = next.getFeatures(0).size(0);
-            for (int predictionIndex = 0; predictionIndex < numExamples; predictionIndex++) {
-                predictions.clear();
-                for (int outputIndex = 0; outputIndex < domainDescriptor.getNumModelOutputs(); outputIndex++) {
+                MultiDataSet next = iterator.next();
+                INDArray[] outputs = graph.output(next.getFeatures());
+                double dsScore = graph.score(next);
+                if (dsScore == dsScore) {
+                    scoreObserver.accept(dsScore);
+                }
+                INDArray[] trueLabels = next.getLabels();
 
-                    if (interpretors[outputIndex] != null) {
-                        Prediction prediction = interpretors[outputIndex].interpret(
-                                trueLabels[outputIndex],
-                                outputs[outputIndex],
-                                predictionIndex);
-                        prediction.outputIndex = outputIndex;
-                        prediction.index = index;
-                        predictions.add(prediction);
+                int numExamples = next.getFeatures(0).size(0);
+                for (int predictionIndex = 0; predictionIndex < numExamples; predictionIndex++) {
+                    predictions.clear();
+                    for (int outputIndex = 0; outputIndex < domainDescriptor.getNumModelOutputs(); outputIndex++) {
+
+                        if (interpretors[outputIndex] != null) {
+                            Prediction prediction = interpretors[outputIndex].interpret(
+                                    trueLabels[outputIndex],
+                                    outputs[outputIndex],
+                                    predictionIndex);
+                            prediction.outputIndex = outputIndex;
+                            prediction.index = index;
+                            predictions.add(prediction);
+                        }
                     }
+                    GenotypePrediction gp = (GenotypePrediction) domainDescriptor.aggregatePredictions(null/**null in training phase */, predictions);
+                    // obtain the reference base as an int (e.g., 0 or 1), to match the format of
+                    // genotypes obtained during training from the cache:
+                    String referenceBase = Integer.toString(gp.referenceGobyIndex);
+                    accumulator.observe(gp, gp.isVariant(), GenotypeHelper.isVariant(gp.predictedGenotype, referenceBase));
+                    observer.accept(gp);
+                    if (stopIfTrue.test(nProcessed)) {
+                        break;
+                    }
+                    nProcessed += 1;
+                    index++;
                 }
-                GenotypePrediction gp = (GenotypePrediction) domainDescriptor.aggregatePredictions(null/**null in training phase */, predictions);
-                // obtain the reference base as an int (e.g., 0 or 1), to match the format of
-                // genotypes obtained during training from the cache:
-                String referenceBase = Integer.toString(gp.referenceGobyIndex);
-                accumulator.observe(gp, gp.isVariant(), GenotypeHelper.isVariant(gp.predictedGenotype, referenceBase));
-                observer.accept(gp);
-                if (stopIfTrue.test(nProcessed)) {
-                    break;
-                }
-                nProcessed += 1;
-                index++;
+                ws.notifyScopeLeft();
             }
         }
-
         return accumulator.createOutputStatistics()[StatsAccumulator.F1_INDEX];
     }
 
