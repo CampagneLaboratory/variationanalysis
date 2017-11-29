428fa04d5b97e6b05f0bf1c51875aeb959f3da74
Fabien Campagne <fac2003@campagnelab.org>
2017-11-29 12:07:56 -0500

    Extend argument generator to support optional command line flags.

--- bin/arg-generator.sh
+++ bin/arg-generator.sh
@@ -2,4 +2,5 @@
 FORCE_PLATFORM="native"
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
-java -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG}  org.campagnelab.dl.framework.tools.ArgGenerator ${other_parameters}
+java -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
+ org.campagnelab.dl.framework.tools.ArgGenerator ${other_parameters}
