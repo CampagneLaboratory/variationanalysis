95bf1b96a665abdcd0dab89f1f05db2bd4603cd1
Manuele Simi <manuele.simi@campagnelab.org>
2017-12-08 17:01:05 -0500

    Prototype of the SSIFilter tool. This version writes all the segments in the specified range. Still largely untested.

---
+++ genotype/src/main/java/org/campagnelab/dl/genotype/tools/SSIFilter.java
@@ -0,0 +1,64 @@
+package org.campagnelab.dl.genotype.tools;
+
+import org.campagnelab.dl.framework.tools.arguments.AbstractTool;
+import org.campagnelab.dl.genotype.storage.SegmentReader;
+import org.campagnelab.dl.genotype.storage.SegmentWriter;
+import org.campagnelab.dl.varanalysis.protobuf.SegmentInformationRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import play.mvc.WebSocket;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Filter SSI to extract a subset of the segments at specific positions.
+ */
+public class SSIFilter extends AbstractTool<SSIFilterArguments> {
+
+    static private Logger LOG = LoggerFactory.getLogger(SSIFilter.class);
+    int startLocation;
+    int endLocation;
+    String referenceId;
+
+    public static void main(String[] args) {
+        SBIToSSIConverter tool = new SBIToSSIConverter();
+        tool.parseArguments(args, "SSIFilter", tool.createArguments());
+        tool.execute();
+    }
+
+    @Override
+    public SSIFilterArguments createArguments() {
+        return new SSIFilterArguments();
+    }
+
+    @Override
+    public void execute() {
+        String[] start = this.args().startPosition.split(":", 2);
+        this.referenceId = start[0];
+        this.startLocation = Integer.valueOf(start[1]);
+        String[] end = this.args().endPosition.split(":", 2);
+        this.endLocation = Integer.valueOf(end[1]);
+        if (!Objects.equals(this.referenceId, end[0]))
+            throw new IllegalArgumentException("Chromosomes in the start and end positions do not match.");
+        try (SegmentReader ssiReader = new SegmentReader(new File(args().inputFile).getAbsolutePath());
+             SegmentWriter ssiwriter = new SegmentWriter(new File(args().outputFile).getAbsolutePath());) {
+            ssiReader.forEach(segmentInformation -> {
+                if (this.accept(segmentInformation)) {
+                    ssiwriter.writeRecord(segmentInformation);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean accept(SegmentInformationRecords.SegmentInformation segment) {
+        if (!Objects.equals(segment.getStartPosition().getReferenceId(), this.referenceId))
+            return false;
+        return (segment.getStartPosition().getLocation() >= this.startLocation
+                || segment.getEndPosition().getLocation() <= this.endLocation);
+    }
+}
---
+++ genotype/src/main/java/org/campagnelab/dl/genotype/tools/SSIFilterArguments.java
@@ -0,0 +1,28 @@
+package org.campagnelab.dl.genotype.tools;
+
+import com.beust.jcommander.Parameter;
+import org.campagnelab.dl.framework.tools.arguments.ToolArguments;
+
+/**
+ * Created by mas2182 on 12/8/17.
+ */
+public class SSIFilterArguments implements ToolArguments {
+
+    @Parameter(required = true, names = {"-i", "--input-file"}, variableArity = true,
+            description = "Input SSI to filter, must be provided in .ssi format.")
+    public String inputFile = null;
+
+    @Parameter(required = true, names = {"-o", "--output-file"}, variableArity = true,
+            description = "Output SSI with the filtered records.")
+    public String outputFile = null;
+
+    @Parameter(required = true, names = {"-s", "--start-position"}, variableArity = true,
+            description = "Position from which the filter operates, must be provided in chr:pos format.")
+    public String startPosition = null;
+
+
+    @Parameter(required = true, names = {"-s", "--start-position"}, variableArity = true,
+            description = "Position at which the filter stops, must be provided in chr:pos format.")
+    public String endPosition = null;
+
+}
