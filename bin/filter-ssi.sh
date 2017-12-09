032fe2321d695ad4703f2103b58492ce6e71e16f
Manuele Simi <manuele.simi@campagnelab.org>
2017-12-09 11:53:09 -0500

    Add script to invoke the FilterSSI tool.

---
+++ bin/filter-ssi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+FORCE_PLATFORM=native
+. `dirname "${BASH_SOURCE[0]}"`/setup.sh
+
+java -ea -Xmx${memory_requirement} -cp ${GDLVA_JAR} -Dlogback.configurationFile=${SLF4J_CONFIG} \
+    org.campagnelab.dl.genotype.tools.FilterSSI ${other_parameters}
