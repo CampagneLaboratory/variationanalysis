b89548e092df44aecdbc025350d01767984d5eb9
Fabien Campagne <fac2003@campagnelab.org>
2018-01-11 13:50:10 -0500

    Fix for gatk-realign wrapper script.

--- bin/parallel-gatk-realign.sh
+++ bin/parallel-gatk-realign.sh
@@ -4,10 +4,10 @@ DISTRIBUTION=$1
 shift
 set -x
 
-if [ -f ${DISTRIBUTION}/gatk ]; then
-    bin/parallel-gatk4-realign-filtered.sh $1/gatk "$@"
-elif [ -f ${DISTRIBUTION}/GenomeAnalysisTK.jar ]; then
-    bin/parallel-gatk3-realign-filtered.sh $1/GenomeAnalysisTK.jar "$@"
+if [ -e "${DISTRIBUTION}/gatk" ]; then
+    bin/parallel-gatk4-realign-filtered.sh ${DISTRIBUTION}/gatk "$@"
+elif [ -e "${DISTRIBUTION}/GenomeAnalysisTK.jar" ]; then
+    bin/parallel-gatk3-realign-filtered.sh ${DISTRIBUTION}/GenomeAnalysisTK.jar "$@"
 else
     echo "Unable to execute GATK distribution at ${DISTRIBUTION}"
 fi
