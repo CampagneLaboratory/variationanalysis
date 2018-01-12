5a3dd20720ffa4abe1cf45d4878d510914b6d8fd
Fabien Campagne <fac2003@campagnelab.org>
2018-01-11 22:03:11 -0500

    Update rebuild script.

--- bin/rebuild-segment-datasets.sh
+++ bin/rebuild-segment-datasets.sh
@@ -3,5 +3,11 @@
 (cd ~/goby3; git stash; git pull; mvn install)
 (cd ~/variationanalysis; git stash; git pull; ./build-all.sh -DskipTests=true)
 DATE=`date +%Y-%m-%d`
-sbi-to-ssi.sh 10g -i out-part-11.sbi -o training-${DATE} -g 10 --map-features -s INDEL1 --sampling-rate 0.01
-sbi-to-ssi.sh 10g -i out-part-41.sbi -o validation-${DATE} -g 10 --map-features -s INDEL1 --sampling-rate 0.01
+PREFIX="NA12878-GIAB-chr16-chr18-chr20"
+sbi-to-ssi.sh 10g -i "${PREFIX}-training.sbi" -o "${PREFIX}-training-${DATE}" -g 10 --map-features -s INDEL1 --sampling-rate 0.01 $@
+sbi-to-ssi.sh 10g -i "${PREFIX}-validation.sbi" -o "${PREFIX}-validation-${DATE}" -g 10 --map-features -s INDEL1 --sampling-rate 0.01 $@
+sbi-to-ssi.sh 10g -i "${PREFIX}-test.sbi" -o "${PREFIX}-validation-${DATE}" -g 10 --map-features -s INDEL1 --sampling-rate 0.01 $@
+
+randomize-ssi.sh 10g -i "${PREFIX}-training-${DATE}" -o "${PREFIX}-random-${DATE}-train"
+randomize-ssi.sh 10g -i "${PREFIX}-validation-${DATE}" -o "${PREFIX}-random-${DATE}-validation"
+randomize-ssi.sh 10g -i "${PREFIX}-test-${DATE}" -o "${PREFIX}-random-${DATE}-test"
