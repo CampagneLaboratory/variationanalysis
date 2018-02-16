80c04c2775e6355aed5e0295001232b5c81cf4a7
Joshua Cohen <jbc264@cornell.edu>
2018-02-16 10:51:11 -0500

    Add MAX_RECORDS back to predict-dataset

--- bin/evaluate-genotypes-vec.sh
+++ bin/evaluate-genotypes-vec.sh
@@ -171,7 +171,7 @@ if [ -z "${VCF_OUTPUT+set}" ] || [ -z "${BED_OBSERVED_REGIONS_OUTPUT+set}" ]; th
     predict-dataset.sh --model-path ${MODEL_DIR} --model-label ${MODEL_PREFIX} \
         --problem "genotyping:${DATASET_BASENAME}" \
         --dataset validation --mini-batch-size ${MINI_BATCH_SIZE} \
-        --output "${DATASET_BASENAME}_predicted" --checkpoint-key ${CHECKPOINT_KEY}
+        --output "${DATASET_BASENAME}_predicted" --checkpoint-key ${CHECKPOINT_KEY} ${PREDICT_MAX_RECORDS}
     dieIfError "Failed to predict dataset with model ${MODEL_DIR}/."
 
     echo "Running predict-genotypes to create VCF and observed region bed.."
