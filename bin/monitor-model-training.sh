d141de45cff84c4f290cacc1ecf7bd1dbeef6a82
Joshua Cohen <jbc264@cornell.edu>
2018-02-22 12:55:29 -0500

    Redirect console output from monitor-model-training to file or devnull

--- bin/monitor-model-training.sh
+++ bin/monitor-model-training.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 USAGE_STR=$(cat <<-END
-    Usage: monitor-model-training.sh -m model-directory -c checkpoint-key -p model-prefix -t test-sbi -d dataset -l log-file
+    Usage: monitor-model-training.sh -m model-directory -c checkpoint-key -p model-prefix -t test-sbi -d dataset [-n]
     Monitor the progress of a model being trained by PyTorch
     -m should point to the directory of the trained PyTorch model being used with a config.properties within.
     -c should be the checkpoint key used and -p should be the model prefix (usually best or latest)
     -t should be a path to the test SBI file, and -d should be the name of the dataset used for the sbi.
+    -n disables writing progress out to a file
 END
 )
 
@@ -33,8 +34,9 @@ CHECKPOINT_KEY=""
 MODEL_PREFIX=""
 DATASET_SBI=""
 DATASET_NAME=""
+NONVERBOSE=0
 
-while getopts ":hm:c:p:t:d:" opt; do
+while getopts ":hm:c:p:t:d:n" opt; do
     case "${opt}" in
         h)
             echo "${USAGE_STR}"
@@ -54,6 +56,9 @@ while getopts ":hm:c:p:t:d:" opt; do
             ;;
         d)
             DATASET_NAME=$OPTARG
+            ;;
+        n)
+            NONVERBOSE=1
             ;;
         \?)
             echo "Invalid option: -${OPTARG}" 1>&2
@@ -92,16 +97,28 @@ echo "Logging changes on ${FULL_MODEL_PATH}..."
 
 PREV_MODEL_MD5=""
 
+LOG_PROGRESS_PATH="${CHECKPOINT_KEY}_progress.log"
+if [ ${NONVERBOSE} -ne 0 ]; then
+    LOG_PROGRESS_PATH="/dev/null"
+fi
+
 # Compare current hash against previous; if different, run evaluate and log results from evaluate to log file
 while sleep 1
 do
     CURR_MODEL_MD5=$(md5sum "${FULL_MODEL_PATH}")
     if [[ "${CURR_MODEL_MD5}" != "${PREV_MODEL_MD5}" ]] ; then
-        echo "Change detected on ${FULL_MODEL_PATH}"
+        echo "=========Change detected on ${FULL_MODEL_PATH}========="
         RANDOM_OUTPUT_SUFFIX="${RANDOM}"
-        evaluate-genotypes-vec.sh -m "${MODEL_DIR}" -c "${CHECKPOINT_KEY}" -p "${MODEL_PREFIX}" -t "${DATASET_SBI}" -d "${DATASET_NAME}" -r "${RANDOM_OUTPUT_SUFFIX}"
-        log-evaluate.sh --model-path "${MODEL_DIR}" --checkpoint-key "${CHECKPOINT_KEY}" --model-label "${MODEL_PREFIX}" --vcf-path "output-${RANDOM_OUTPUT_SUFFIX}" --output-path "${CHECKPOINT_KEY}_log.tsv"
+        NOW=$(DATE)
+        echo "========= NEW EVALUATION RUN ON ${NOW} FOR MODEL ${FULL_MODEL_PATH} ON DATASET ${DATASET_SBI} =========" >> ${LOG_PROGRESS_PATH}
+        echo "Running evaluate on ${FULL_MODEL_PATH} with dataset ${DATASET_SBI}..."
+        evaluate-genotypes-vec.sh -m "${MODEL_DIR}" -c "${CHECKPOINT_KEY}" -p "${MODEL_PREFIX}" -t "${DATASET_SBI}" -d "${DATASET_NAME}" -r "${RANDOM_OUTPUT_SUFFIX}" >>"${LOG_PROGRESS_PATH}" 2>&1
+        dieIfError "Unable to evaluate genotypes with the given parameters"
+        echo "Logging results of evaluate on ${FULL_MODEL_PATH} with dataset ${DATASET_SBI}..."
+        log-evaluate.sh --model-path "${MODEL_DIR}" --checkpoint-key "${CHECKPOINT_KEY}" --model-label "${MODEL_PREFIX}" --vcf-path "output-${RANDOM_OUTPUT_SUFFIX}" --output-path "${CHECKPOINT_KEY}_log.tsv" >>"${LOG_PROGRESS_PATH}" 2>&1
+        dieIfError "Unable to log results of evaluation with the given parameters"
         PREV_MODEL_MD5="${CURR_MODEL_MD5}"
         rm -r "output-${RANDOM_OUTPUT_SUFFIX}"
+        echo "Evaluation and logging for next epoch of ${FULL_MODEL_PATH} complete"
     fi
 done
